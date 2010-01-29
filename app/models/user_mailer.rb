9537f974d77699b407b65cdc5c97af77228f5319
Andy <andy@scalevp.com>
2010-01-28 17:54:34 -0800

    Expand leaderboards, tweak activation email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     @headers = {content_type => 'text/html'}
     setup_email(user)
-    @subject << 'Please activate your Memverse account'
+    @subject = 'Please activate your Memverse account'
     @body[:url] = "#{APP_CONFIG[:site_url]}/activate/#{user.activation_code}"
   end
   
@@ -52,7 +52,7 @@ class UserMailer < ActionMailer::Base
   def setup_email(user)
     @recipients = "#{user.email}"
     @from = APP_CONFIG[:admin_email]
-    @subject = "[#{APP_CONFIG[:site_name]}] "
+    @subject = "#{APP_CONFIG[:site_name]} "
     @sent_on = Time.now
     @body[:user] = user
   end
