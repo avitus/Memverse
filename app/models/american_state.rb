6052613d51ee202282407b84b9d44987fc9a4997
Andy Vitus <andy@scalevp.com>
2010-02-20 23:58:12 -0800

    Added state challenge

--- app/models/american_state.rb
+++ app/models/american_state.rb
@@ -11,4 +11,33 @@ class AmericanState < ActiveRecord::Base
   # Validations
   validates_presence_of :name, :abbrev
   
+  
+  # ----------------------------------------------------------------------------------------------------------
+  # Returns hash of top countries (sorted by number of verses memorized)
+  # ---------------------------------------------------------------------------------------------------------- 
+  def self.top_states(numstates = 50)
+
+    stateboard = Hash.new(0)
+    
+    # TODO: Optimize this - compare to top churches method
+    find(:all, :conditions => ['users_count >= 3']).each { |c|
+      
+      score = 0
+      c.users.each { |u|
+        if u.last_activity_date
+          if (Date.today - u.last_activity_date).to_i < 30
+            # Add score
+            score += u.memorized
+          end
+        end
+      }
+      stateboard[ c ] = score
+    }
+    
+    return stateboard.sort{|a,b| a[1]<=>b[1]}.reverse[0...numstates]
+    
+  end    
+  
+  
+  
 end
