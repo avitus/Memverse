3d21e62c5db6d780d6619173e0c078e63f0ddb7a
Andy <andy@scalevp.com>
2010-04-06 17:38:58 -0700

    Clean up church members view

--- app/controllers/american_state_controller.rb
+++ app/controllers/american_state_controller.rb
@@ -2,10 +2,7 @@ class AmericanStateController < ApplicationController
  
   def show
     @us_state = AmericanState.find(params[:id])
-    logger.debug("*** Found US State: #{@us_state.name}")
     @users    = @us_state.users
-    logger.debug("*** Found #{@users.size} users from this state")
-
   end   
   
 end
