05c4dafd0d552ea7022e27de49a1bad456ee3e62
Andy <andy@scalevp.com>
2010-05-04 17:34:11 -0700

    Struggling with testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
+  before_filter :login_required, :only => :show
   
   def new
     @user = User.new
