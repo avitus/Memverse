430c56838734e35ce79cdb84b1675242a4a880bd
Andy <andy@scalevp.com>
2010-08-04 17:43:22 -0700

    Stop using exception notification plugin

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,4 @@
-admin_email = YAML.load_file("#{RAILS_ROOT}/config/settings.yml")[RAILS_ENV]['admin_email']
-ExceptionNotifier.exception_recipients = admin_email
+# No longer using exception notification - using Exceptional web service instead
+
+#admin_email = YAML.load_file("#{RAILS_ROOT}/config/settings.yml")[RAILS_ENV]['admin_email']
+#ExceptionNotifier.exception_recipients = admin_email
