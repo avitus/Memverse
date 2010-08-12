c4355db6fed79000f1ee76f4904fdc89274d244f
Andy <andy@scalevp.com>
2010-08-12 11:57:00 -0700

    Pushing functionality into user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     
-    quests_to_check = @user.current_uncompleted_quests
-    quests_to_check.each { |q|
-      if q.complete?(@user)
-        q.check_quest_off(@user)
-        flash[:notice] = "Great. You have completed the following task: #{q.task}"
-      end
-    }
+    # Check for completed tasks    
+    if quests = @user.check_for_completed_quests
+      flash[:notice] = "Great. You have completed the following task: #{quests.length}"
+    end
     
     # Has user leveled up?
     if @user.check_for_level_up
-      flash[:notice] = "Congratulations!! You have reached a new level."
+      flash[:notice] = "Congratulations!! You have reached level #{@user.level}."
     end    
     
     @current_user_quests = @user.current_uncompleted_quests
