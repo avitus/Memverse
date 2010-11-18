928addb573dccc9b89b100f613095499a614d3be
Andy <andy@scalevp.com>
2010-11-17 18:03:58 -0800

    Updating Capistrano deployment file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,33 @@
-set :stages, %w(staging production)
-set :default_stage, "production"
-require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+set :user, 'andyvitus'  # Your hosting account's username
+set :domain, 'ps19952.dreamhost.com'  # Hosting servername where your account is located
+set :project, 'Memverse'  # Your application as its called in the repository
+set :application, 'staging.memverse.com'  # Your app's location (domain or subdomain)
+set :applicationdir, "/home/#{user}/#{application}"  # The location of your application on your hosting (may differ for each hosting provider)
+# version control config
+set :scm, 'git'
+set :repository,  "git@github.com:avitus/Memverse.git" # Your git repository location
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1 # if you have vendored rails
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+# deploy config
+set :deploy_to, applicationdir # deploy to directory set above
+set :deploy_via, :export
+# additional settings
+default_run_options[:pty] = true  # Forgo errors when deploying from windows
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :use_sudo, false
 
+# Original below this line ---------
+
+set :stages, %w(staging production)
+set :default_stage, "staging"
+require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
 
 namespace :db do
   desc 'Dumps the production database to db/production_data.sql on the remote server'
