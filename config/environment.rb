282186433bb790fdc199a62cd92eb6593596935e
Andy <andy@scalevp.com>
2010-12-08 05:09:05 +0800

    Change to staging environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 ENV['RAILS_ENV'] ||= 'production'
+RAILS_ENV = 'staging'
 
 # Set path to add local gems on Dreamhost
 ENV['GEM_PATH'] = '/home/andyvitus/.gems:/usr/lib/ruby/gems/1.8'
