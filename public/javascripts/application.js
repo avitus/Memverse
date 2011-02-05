95bf3c3b2c4618f68a5633786984797e8693af4c
Alex Watt <alexcwatt@gmail.com>
2011-02-04 19:29:29 -0500

    Multi-verse delete now with improved JavaScript error messages.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,54 +1,81 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-
 // Thanks to Dustin for post: http://www.dustindiaz.com/check-one-check-all-javascript/
 function checkAllFields(ref)
 {
-	var chkAll = document.getElementById('checkAll');
-	var checks = document.getElementsByName('mv[]');
-	var boxLength = checks.length;
-	var allChecked = false;
-	var totalChecked = 0;
-	
-	if ( ref == 1 )
-	{
-		if ( chkAll.checked == true )
-		{
-			for ( i=0; i < boxLength; i++ )
-			checks[i].checked = true;
-		}
-		else
-		{
-			for ( i=0; i < boxLength; i++ )
-			checks[i].checked = false;
-		}
-	}
-	else
-	{
-		for ( i=0; i < boxLength; i++ )
-		{
-			if ( checks[i].checked == true )
-			{
-			allChecked = true;
-			continue;
-			}
-			else
-			{
-			allChecked = false;
-			break;
-			}
-		}
-		if ( allChecked == true )
-		chkAll.checked = true;
-		else
-		chkAll.checked = false;
-	}
-	for ( j=0; j < boxLength; j++ )
-	{
-		if ( checks[j].checked == true )
-		totalChecked++;
-	}
+var chkAll = document.getElementById('checkAll');
+var checks = document.getElementsByName('mv[]');
+var boxLength = checks.length;
+var allChecked = false;
+var totalChecked = 0;
+    if ( ref == 1 )
+    {
+        if ( chkAll.checked == true )
+        {
+            for ( i=0; i < boxLength; i++ )
+            checks[i].checked = true;
+        }
+        else
+        {
+            for ( i=0; i < boxLength; i++ )
+            checks[i].checked = false;
+        }
+    }
+    if (ref == 2)
+    {
+        for ( i=0; i < boxLength; i++ )
+        {
+            if ( checks[i].checked == true )
+            {
+            allChecked = true;
+            continue;
+            }
+            else
+            {
+            allChecked = false;
+            break;
+            }
+        }
+        if ( allChecked == true )
+        chkAll.checked = true;
+        else
+        chkAll.checked = false;
+    }
+    for ( j=0; j < boxLength; j++ )
+    {
+        if ( checks[j].checked == true )
+        totalChecked++;
+    }
+    if ( ref == 3 )
+    {
+        if ( totalChecked == 1 ) {
+        var agree=confirm("Are you sure you want to delete the selected verse?");
+         if (agree)
+          return true;
+         else
+          return false;
+
+        }
+        else if ( totalChecked == 0 ) {
+        alert("Requested action could not be performed: no verses selected.");
+        return false;
+        }
+        else if ( totalChecked == boxLength ) {
+        var agree=confirm("Are you sure you want to delete all "+totalChecked+" of your verses?");
+         if (agree)
+          return true;
+         else
+          return false;
+        }
+        else {
+        var agree=confirm("Are you sure you want to delete the "+totalChecked+" selected verses?");
+         if (agree)
+          return true;
+         else
+          return false;
+        }
+    }
 }
 
 
