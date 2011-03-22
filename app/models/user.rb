9fecc311534c02f8ccf04598932acc6f1c382cd2
Alex Watt <alexcwatt@gmail.com>
2011-03-22 09:50:19 -0400

    Add Nathan Burkhalter as blogger

--- app/models/user.rb
+++ app/models/user.rb
@@ -936,7 +936,7 @@ class User < ActiveRecord::Base
     #
     # Of course, you could also create a join table to join users to blogs they can blog in.  But do you want to do 
     # that with blog comments and ability to moderate comments as well?
-    self.id == 2 or self.id == 366 or self.id == 1138 or self.id == 3113 or self.id == 4024 or self.id == 3486 # Restrict blogging to me, Heather-Kate Taylor, Phil Walker, Dakota Lynch, River La Belle and Alex Watt
+    self.id == 2 or self.id == 366 or self.id == 1138 or self.id == 3113 or self.id == 4024 or self.id == 3486 or self.id == 4565 # Restrict blogging to me, Heather-Kate Taylor, Phil Walker, Dakota Lynch, River La Belle, Alex Watt and Nathan Burkhalter
   end
   
   # Whether a user can moderate the comments for a given blog
