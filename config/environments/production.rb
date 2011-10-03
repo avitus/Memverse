f699c4ca6d56013085c335fa209aa5a9e68770fe
¨Andy¨ <andy@scalevp.com>
2011-10-03 13:14:09 -0700

    Precompile Rails Admin assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,8 @@ MemverseApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w(rails_admin/rails_admin.js rails_admin/rails_admin.css) # This is a temporary workaround until Rails 3.1.1 Should be able to remove
+  
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
