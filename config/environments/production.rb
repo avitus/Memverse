18c10d31cbaca456b80bdd786ff9f8d7ef35a691
¨Andy¨ <andy@scalevp.com>
2011-10-03 13:42:50 -0700

    Fallback to assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ MemverseApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = false # This should be false but seems to be a problem with Rails Admin ... try again with Rails 3.1.3
 
   # Generate digests for assets URLs
   config.assets.digest = true
