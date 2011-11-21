458d68cbc146682e341c7481f1a6f88ef0034e63
¨Andy¨ <andy@scalevp.com>
2011-11-21 14:21:35 -0800

    Specify domain for email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ MemverseApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'yourhost.com' }
+  config.action_mailer.default_url_options = { :host => 'memverse.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
