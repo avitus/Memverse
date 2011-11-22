c554e7839a282086cd043df0425e3734d86629b6
¨Andy¨ <andy@scalevp.com>
2011-11-22 10:04:03 -0800

    Email finally working ... no need to raise exception on errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ MemverseApp::Application.configure do
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
     
   config.action_mailer.delivery_method = :smtp
