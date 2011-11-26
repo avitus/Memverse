70d527fdec8cc0eafdc255d7e5907d618b7e882a
¨Andy¨ <andy@scalevp.com>
2011-11-26 13:50:50 -0800

    Cache pop verse illumination and recent blog posts on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,15 @@ class HomeController < ApplicationController
   def index
     redirect_to :controller => "memverses", :action => "index" and return if user_signed_in?
     session[:referrer] = params[:referrer]
-    @blogposts = BlogPost.all(:limit => 2, :order => "created_at DESC", :conditions => ["is_complete = ?", true])
-    @vsillumination = Popverse.random(10)
+     
+    @blogposts = Rails.cache.fetch(["latest_blog_posts"], :expires_in => 2.hours) do 
+      BlogPost.where(:is_complete => true).order("created_at DESC").limit(2)
+    end
+    
+    @vsillumination = Rails.cache.fetch(["pop_vs_illumination"], :expires_in => 24.hours) do 
+      Popverse.random(10)
+    end
+    
     render :layout => false
   end  
    
