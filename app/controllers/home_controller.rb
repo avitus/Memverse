0eb02d06e848c8f0b69a04659dc042230c40c480
¨Andy¨ <andy@scalevp.com>
2011-11-27 11:04:48 -0800

    Disable home page cache for now ... error about frozen object

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,17 @@ class HomeController < ApplicationController
     redirect_to :controller => "memverses", :action => "index" and return if user_signed_in?
     session[:referrer] = params[:referrer]
      
-    @blogposts = Rails.cache.fetch(["latest_blog_posts"], :expires_in => 2.hours) do 
-      BlogPost.where(:is_complete => true).order("created_at DESC").limit(2).all
-    end
-    
-    @vsillumination = Rails.cache.fetch(["pop_vs_illumination"], :expires_in => 24.hours) do 
-      Popverse.random(10)
-    end
+    # TODO: Caching these requests gives an error about a frozen object 
+    # @blogposts = Rails.cache.fetch(["latest_blog_posts"], :expires_in => 2.hours) do 
+      # BlogPost.where(:is_complete => true).order("created_at DESC").limit(2)
+    # end
+    @blogposts = BlogPost.where(:is_complete => true).order("created_at DESC").limit(2)     
+
+
+    # @vsillumination = Rails.cache.fetch(["pop_vs_illumination"], :expires_in => 24.hours) do 
+      # Popverse.random(10)
+    # end
+    @vsillumination = Popverse.random(10)
     
     render :layout => false
   end  
