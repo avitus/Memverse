1783856ba8c0f860f707da511c81e4e976aa227f
¨Andy¨ <andy@scalevp.com>
2011-12-04 14:24:37 -0800

    Run task to symlink bloggity upload folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,8 +67,8 @@ set :default_stage, "production"
 ##  Hooks
 ##############################################################
 # after "deploy:update_code", "deploy:symlink_db" #, "deploy:set_rails_env"
-before "deploy:assets:precompile", "deploy:symlink_db"
-
+before "deploy:assets:precompile", "deploy:symlink_db", 
+after  "deploy:symlink_bloggity"
 
 ##############################################################
 ##  Database config and restart
