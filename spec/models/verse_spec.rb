e2f439f802cb606a5073ad49776ed263a9bdb417
Alex Watt <alexcwatt@gmail.com>
2012-01-18 11:02:46 -0500

    Failing spec for verse cleanup

--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -10,5 +10,10 @@ describe Verse do
     verse = Factory(:verse, :text => "Á têst for mnémonic support of thé íncredíbly speciâl Portuguese çharacters.")
     verse.mnemonic.should == "Á t f m s o t í s P ç."
   end
-
+  
+  it "should clean up the verse text" do
+    verse = Factory(:verse, :text => "This is a \r\n test test   \n   test. ")
+	verse.save!
+    verse.text.should == "This is a test test test."
+  end
 end
