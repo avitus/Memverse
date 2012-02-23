a3905f2971117af509328d71a98481297cf66776
¨Andy¨ <andy@scalevp.com>
2012-02-22 17:25:01 -0800

    Create rest of quests - not yet complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,33 @@ user = User.create! :name => 'First User', :email => 'user@test.com', :password 
 puts 'New user created: ' << user.name
 
 # TODO: Add popular verses
+
+# Add 50 quest levels
+puts 'SETTING UP QUESTS'
+for i in 10..50
+  learning_verses   = (i-6)*25 + 50
+  memorized_verses  = learning_verses - 50
+  
+  gospel_learning   = (learning_verses.to_f / 10.0).round
+  epistle_learning  = (learning_verses.to_f / 10.0).round
+  wisdom_learning   = (learning_verses.to_f / 20.0).round
+  history_learning  = (learning_verses.to_f / 20.0).round
+  prophecy_learning = (learning_verses.to_f / 20.0).round
+
+  gospel_memorized   = (memorized_verses.to_f / 10.0).round
+  epistle_memorized  = (memorized_verses.to_f / 10.0).round
+  wisdom_memorized   = (memorized_verses.to_f / 20.0).round
+  history_memorized  = (memorized_verses.to_f / 20.0).round
+  prophecy_memorized = (memorized_verses.to_f / 20.0).round
+  
+  accuracy           = i + 15
+  ref_recall         = [i + 50, 90].min
+  
+  referrals          = ((i-6).to_f / 3.0).round
+  tags               = (learning_verses.to_f / 5.0).round
+  
+  chapters_learning  = ((i-5).to_f / 3.0).round
+  chapters_memorized = ((i-7).to_f / 3.0).round
+
+  
+end
