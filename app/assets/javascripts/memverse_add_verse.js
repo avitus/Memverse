2199335e55354c1879f7e68f25c33e3f119ac524
¨Andy¨ <andy@scalevp.com>
2012-03-08 11:30:25 -0800

    Progress on new add_verse page

--- app/assets/javascripts/memverse_add_verse.js
+++ app/assets/javascripts/memverse_add_verse.js
@@ -2,25 +2,60 @@ $(document).ready(function() {
 
 	// Verse entry and retrieval
 	$(".flex-verse-search").observe_field(0.2, function( ) { 
-								
+		
+		// User is looking for a single verse						
 		if (ref = parseVerseRef($.trim(this.value))) {
 			$.get("/lookup_verse.json", { bk: ref.bk, ch: ref.ch, vs: ref.vs },
-				function(data) {	
+				function(verse) {	
 
-			}, "json" );			
+					$("#foundVerse").empty();
+					if (typeof(verse) !== 'undefined' && verse != null) {
+						$("#new-verse-entry").hide();												
+						$("#foundVerse").append($('<h4/>').text(verse.ref)).append($('<p/>').text(verse.text));
+						$("#quick-start-add-verse").html("<a data-remote='true' href='/add/" + verse.id + "'class='quick-start-add-button' id='quick-start-add'></a>");
+					} else {					
+						$("#new-verse-entry").slideDown();																
+						$("#quick-start-add-verse").empty();
+						$("#new-verse-entry .quick-start-add-verse").html("<div class='quick-start-add-button'></div>")  // show add button
+					};
+
+
+			}, "json" );	
+					
+		// User is searching for a passage
 		} else if (ref = parsePassageRef($.trim(this.value))) {
 			$.get("/lookup_passage.json", { bk: ref.bk, ch: ref.ch, vs_start: ref.vs_start, vs_end: ref.vs_end },
-				function(data) {
+				function(verses) {
 					
-				// Insert missing verses with option to create a new verse
-				
-				// Display verses
+					// Insert missing verses with option to create a new verse
+					
+					// Clear existing search results
+					$('.pop-verse-group').empty();
+					
+					// Display verses
+					$.each (verses, function(i, pv) {
+						// We need to group popular verses		
+						if (i % 3 == 0) {
+							// Start new group
+							var $new_vs_group = $('<div/>').addClass('pop-verse-group');
+							$('.items').append($new_vs_group);
+						} 
+						var $new_pv = $('<div/>').addClass('item quick-start-show-verse')
+							.append($('<h4/>').text(pv.ref))
+							.append($('<p/>').text(pv.text))
+							.append('<div class="quick-start-add-verse"><a data-remote="true" href="/add/' + pv.id + '" class="quick-start-add-button" id="quick-start-add"></a></div>');
+						$('.pop-verse-group').filter(':last').append($new_pv);																
+					});
+					
+					// initialize scrollable module
+					$(".scrollable").scrollable({ vertical: true, mousewheel: true });	
 					
 			}, "json" );
+		
+		// User didn't enter a verse reference ... do a keyword search
 		} else {
-			// User didn't enter a verse reference ... do a keyword search
 			$.get("/search_verse.json", { searchParams: $.trim(this.value)  },
-				function(data) {
+				function(verses) {
 					
 			}, "json" );
 		}			
