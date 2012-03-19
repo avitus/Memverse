98cb2360c7cb89cae7fde2bbfa3cad4b351dd3ad
Alex Watt <alexcwatt@gmail.com>
2012-03-19 14:05:04 -0400

    Add jquery ui to home page; now memverse_lib.js call to autocomplete() is valid.
    Locally seems to fix blmeckle's reported issue here: https://getsatisfaction.com/memverse/topics/memverse_com_webpage_and_login_page

--- app/assets/javascripts/memverse_lib.js
+++ app/assets/javascripts/memverse_lib.js
@@ -203,7 +203,7 @@ $(document).ready(function() {
                 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew',
                 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians',
                 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
-                '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']						
+                '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
 	});	
 	
 });
