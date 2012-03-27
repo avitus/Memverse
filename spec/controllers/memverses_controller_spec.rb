667143abad0fd3602fe541bc7d43cebbfdf3f63c
¨Andy¨ <andy@scalevp.com>
2012-03-26 22:00:49 -0700

    Not sure why this test fails

--- spec/controllers/memverses_controller_spec.rb
+++ spec/controllers/memverses_controller_spec.rb
@@ -28,7 +28,8 @@ describe MemversesController do
       @verse_niv = Factory(:verse, :translation => 'NIV')
       @verse_esv = Factory(:verse, :translation => 'ESV')
       get :ajax_add, :id => @verse_niv
-      get :ajax_add, :id => @verse_esv # This test is currently failing since it can't save the Memverse object it makes with this request
+      get :ajax_add, :id => @verse_esv      # This test is currently failing since it can't save the Memverse object it makes with this request
+                                            # Why does this cause the test to fail? It doesn't throw an exception so the test should pass.
       @user.memverses.count.should == 1            
     end
     
