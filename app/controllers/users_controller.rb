53123155a51f240f71c78814e574bc28a569ce89
¨Andy¨ <andy@scalevp.com>
2012-04-18 14:55:04 -0700

    Remove unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,10 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, :only => :show
   
   add_breadcrumb "Home", :root_path
-  
-  def new
-    @user = User.new
-  end
 
+  # ----------------------------------------------------------------------------------------------------------
+  # Show user account page
+  # ----------------------------------------------------------------------------------------------------------   
   def show
     @tab = "profile"
     @sub = "dash" 	
@@ -41,65 +40,15 @@ class UsersController < ApplicationController
     end
     
   end 
- 
-  def create
-    logout_keeping_session!
-    create_new_user(params[:user])
-    # if using_open_id?
-    #   authenticate_with_open_id(params[:openid_url], :return_to => open_id_create_url, 
-    #     :required => [:nickname, :email]) do |result, identity_url, registration|
-    #     if result.successful?
-    #       create_new_user(:identity_url => identity_url, :login => registration['nickname'], :email => registration['email'])
-    #     else
-    #       failed_creation(result.message || "Sorry, something went wrong")
-    #     end
-    #   end
-    # else
-    #   create_new_user(params[:user])
-    # end
-  end
-  
-  
-  # TODO: no longer needed now that we're using Devise (?)
-  def activate
-    logout_keeping_session!
-    user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
-    case
-    when (!params[:activation_code].blank?) && user && !user.active?
-      user.activate!
-      flash[:notice] = "Signup complete! Please sign in to continue."
-      redirect_to new_user_session_path
-    when params[:activation_code].blank?
-      flash[:error] = "The activation code was missing.  Please follow the URL from your email."
-      redirect_back_or_default(root_path)
-    else 
-      flash[:error]  = "We couldn't find a user with that activation code -- check your email? Or maybe you've already activated -- try signing in."
-      redirect_back_or_default(root_path)
-    end
-  end
 
+  # ----------------------------------------------------------------------------------------------------------
+  # Reset memorization schedule
+  # ----------------------------------------------------------------------------------------------------------   
   def reset_schedule
   	due_today = current_user.reset_memorization_schedule
   	render :json => {:due_verses => due_today }
   end
   
   protected
-  
-  def create_new_user(attributes)
-    @user = User.new(attributes)
-    if @user && @user.valid?
-      if @user.not_using_openid?
-        @user.register!
-      else
-        @user.register_openid!
-      end
-    end
     
-    if @user.errors.empty?
-      successful_creation(@user)
-    else
-      failed_creation
-    end
-  end
-  
 end
