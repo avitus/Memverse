67be18e03fd0e1607fed662eec376a31e58b837a
¨Andy¨ <andy@scalevp.com>
2012-04-18 17:01:34 -0700

    Basic forum, problem with current_user path on main layout

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,8 +1,13 @@
 Forem.user_class = "User"
-Forem.email_from_address = "please-change-me@example.com"
+Forem.email_from_address = "admin@memverse.com"
+
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
+
 Forem.per_page = 20
+
+
+Forem.user_profile_links = true
 
 # If you want to change the layout that Forem uses, uncomment and customize these lines:
 #
@@ -11,3 +16,4 @@ Forem.per_page = 20
 # end
 #
 # By default, these lines will use the layout located at app/views/layouts/forem.html.erb in your application.
+
