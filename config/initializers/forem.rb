2d1d96027f4a1260fb1fa7730434382f5c1edcbc
¨Andy¨ <andy@scalevp.com>
2012-04-19 10:24:54 -0700

    Tweak styles, match blog avatar

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,12 +1,7 @@
-Forem.user_class = "User"
+Forem.user_class         = "User"
 Forem.email_from_address = "admin@memverse.com"
-
-# If you do not want to use gravatar for avatars then specify the method to use here:
-# Forem.avatar_user_method = :custom_avatar_url
-
-Forem.per_page = 20
-
-
+Forem.avatar_user_method = :blog_avatar_url  # ALV: match blog avatar
+Forem.per_page           = 20
 Forem.user_profile_links = true
 
 # If you want to change the layout that Forem uses, uncomment and customize these lines:
