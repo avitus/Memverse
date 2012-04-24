0507cd5b83768f12bbdab9a691a063965dcf6e4e
¨Andy¨ <andy@scalevp.com>
2012-04-24 16:58:21 -0700

    Forem style tweaks

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,7 +1,7 @@
 Forem.user_class         = "User"
 Forem.email_from_address = "admin@memverse.com"
 Forem.avatar_user_method = :blog_avatar_url  # ALV: match blog avatar
-Forem.per_page           = 20
+Forem.per_page           = 25
 Forem.user_profile_links = true
 
 # If you want to change the layout that Forem uses, uncomment and customize these lines:
