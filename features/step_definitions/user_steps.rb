26e65308ea0aae5e0470bddeda78b338d23be599
Alex Watt <alexcwatt@gmail.com>
2012-04-28 07:57:32 -0400

    Working on tag verse feature test
    http://stackoverflow.com/questions/6355586/testing-jquery-autocomplete-ui-with-cucumber

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -127,3 +127,11 @@ Then /^the tag "(.*)" should exist for memverse #([0-9]+)$/ do |tagname, id|
   mv = Memverse.find(id)
   mv.tags.include? tagname
 end
+
+When /^I type in "([^\"]*)" into autocomplete list "([^\"]*)" and I choose "([^\"]*)"$/ do |typed, input_name,should_select|
+   page.driver.browser.execute_script %Q{ $('input[data-autocomplete]').trigger("focus") }
+   fill_in("#{input_name}",:with => typed)
+   page.driver.browser.execute_script %Q{ $('input[data-autocomplete]').trigger("keydown") }
+   sleep 1
+   page.driver.browser.execute_script %Q{ $('.ui-menu-item a:contains("#{should_select}")').trigger("mouseenter").trigger("click"); }
+end
