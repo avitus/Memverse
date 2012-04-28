45028f4b1829aa84ebe27a329a39d9075c194842
Alex Watt <alexcwatt@gmail.com>
2012-04-27 22:01:46 -0400

    My attempt to tweak blog comment markup and styling

--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -2,19 +2,22 @@
 describe BlogPost do
 
   before(:each) do
-    @blog = Factory.create(:blog, :title => "Memverse Blog")
-	@user = Factory.create(:user, :id => 2) # user with id of 2 will be allowed to post
+    @blog = FactoryGirl.create(:blog, :title => "Memverse Blog")
+    @user = FactoryGirl.create(:user, :id => 2) # user with id of 2 will be allowed to post
   end
 
   it "should give URL identifier" do
-    post = Factory.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
+    post = FactoryGirl.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
     post.url_identifier.should == "scripture-memorization"
   end
 
   it "should not give the same URL identifier" do
-    post1 = Factory.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
+    post1 = FactoryGirl.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
     post1.url_identifier.should == "scripture-memorization"
-    # post2 = Factory.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
-    # post2.url_identifier.should == "scripture-memorization--1"
+    pp post1.id
+
+    post2 = FactoryGirl.create(:blog_post, :title => "Scripture Memorization", :blog_id => @blog.id, :posted_by => @user)
+    post2.url_identifier.should == "scripture-memorization--1"
+    pp post2.id
   end
 end
