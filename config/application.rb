d8e6c123c3ac744cb1afc6aab9880bad9ecf4e14
¨Andy¨ <andy@scalevp.com>
2012-06-06 17:54:27 -0700

    Fix pagination bug when displaying pending posts

--- config/application.rb
+++ config/application.rb
@@ -43,9 +43,6 @@ module MemverseApp
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  
-    # Ensure that Ckeditor assets are precompiled
-    config.assets.precompile += Ckeditor.assets
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
