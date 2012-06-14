faeea14fcecda0ce335c28fe3c8174a7c6a86b26
Alex Watt <alexcwatt@gmail.com>
2012-06-14 12:53:07 -0400

    Copy ESV verses into ESV07 -- close #136

---
+++ db/migrate/20120614163356_get_esv07_started.rb
@@ -0,0 +1,18 @@
+class GetEsv07Started < ActiveRecord::Migration
+  def up
+    verses = Verse.where(:translation => "ESV")
+    for verse in verses
+      Verse.new(
+        :translation => "ESV07",
+        :book_index  => verse.book_index,
+        :book        => verse.book,
+        :chapter     => verse.chapter,
+        :versenum    => verse.versenum,
+        :text        => verse.text
+      ).save
+    end
+  end
+
+  def down
+  end
+end
