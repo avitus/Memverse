13920dd3607bf52bde75034515840ee47a27ff53
¨Andy¨ <andy@scalevp.com>
2012-06-14 22:32:40 -0700

    Starting on group description edit test

---
+++ features/step_definitions/group_steps.rb
@@ -0,0 +1,10 @@
+Given /^a group called "([^"]*)"$/ do |group_name|
+  Group.new(:name => group_name).save!
+end
+
+Given /^the normal user belongs to the group called "([^"]*)"$/ do |group_name|
+  user = User.find_by_email('user@test.com')
+  grp  = Group.find_by_name(group_name)
+  user.group = grp
+  user.save!
+end
