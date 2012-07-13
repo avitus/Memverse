cff6c7840e84d24cc00086d975f14761163ba900
Alex Watt <alexcwatt@gmail.com>
2012-07-13 15:49:36 -0400

    Untested attempt to add quiz_length to quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -16,5 +16,22 @@ class Quiz < ActiveRecord::Base
 
   # ============= Protected below this line ==================================================================
   protected
-  
+
+  def update_length
+    length = 0
+
+    for question in self.quiz_questions
+      case question.question_type
+      when "recitation"
+        length = length + (question.passage_translations.first.last.split(" ").length * 2.5 + 15.0).to_i + 1 # 24 WPM typing speed with 15 seconds to think
+      when "reference"
+        length = length + 25 + 1
+      when "mcq"
+        length = lenth + 30 + 1
+      end
+      # Note that 1 is added to each of these because we put a 1 second gap between questions
+
+    self.quiz_length = length
+	self.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120708014400) do
+ActiveRecord::Schema.define(:version => 20120713194128) do
 
   create_table "american_states", :force => true do |t|
     t.string  "abbrev",      :limit => 20, :default => "", :null => false
