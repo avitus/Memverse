a1593eaa51a5de8836268c5dd8618fc9419e1dd6
Andy <avitus@gmail.com>
2012-09-20 11:25:46 -0700

    Display search results mostly done

--- app/assets/javascripts/memverse_practice.js
+++ app/assets/javascripts/memverse_practice.js
@@ -2,6 +2,8 @@
  * Display compact list of verse references
  ******************************************************************************/
 function displayMvSearchResultsFn( verses ) {
+    resetScrollable();
+
     // if we have an array
     $(".mv-search-results-compact #actions a, .mv-search-results-compact div.scrollable, .mv-search-results-compact").show();
     
@@ -10,18 +12,22 @@ function displayMvSearchResultsFn( verses ) {
     $.each (verses, function(i, pv) {
         
         // We need to group search results to enable scroll      
-        if (i % 10 == 0) {
+        if (i % 7 == 0) {
             // Start new group
             var $new_vs_group = $('<div/>').addClass('search-result-group');
             $('.items').append($new_vs_group);
         }
+
+        // Show start of verse
+        var shortText = jQuery.trim(pv.text).substring(0, 35).split(" ").slice(0, -1).join(" ") + "...";
 
         // Build HTML for each verse
         var $new_pv = $('<div/>').addClass('item')
             
             // verse reference
             .append( $('<div class="ref-and-first-words" />')
-                .append($('<h4/>').text(pv.ref)) 
+                .append($('<h4/>').text(pv.ref))
+                .append($('<p/>').text( shortText )) 
             )
             
             // add button
