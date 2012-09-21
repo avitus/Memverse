9b956c27df5612feb334d73132c7d3886f870de1
Andy <avitus@gmail.com>
2012-09-20 17:30:39 -0700

    Start work on verse queue

--- app/assets/javascripts/memverse_practice.js
+++ app/assets/javascripts/memverse_practice.js
@@ -4,12 +4,14 @@
 function displayMvSearchResultsFn( verses ) {
     resetScrollable();
 
-    // if we have an array
     $(".mv-search-results-compact #actions a, .mv-search-results-compact div.scrollable, .mv-search-results-compact").show();
     
-    /*-- $("#verse-search-single-result").hide(); --*/
-    
-    $.each (verses, function(i, pv) {
+    // wrap single verse in array
+    if( !($.isArray(verses)) ) {
+        verses = [ verses ]; 
+    }
+
+    $.each (verses, function(i, vs) {
         
         // We need to group search results to enable scroll      
         if (i % 7 == 0) {
@@ -19,25 +21,25 @@ function displayMvSearchResultsFn( verses ) {
         }
 
         // Show start of verse
-        var shortText = jQuery.trim(pv.text).substring(0, 35).split(" ").slice(0, -1).join(" ") + "...";
+        var shortText = jQuery.trim(vs.text).substring(0, 35).split(" ").slice(0, -1).join(" ") + "...";
 
         // Build HTML for each verse
-        var $new_pv = $('<div/>').addClass('item')
+        var $new_vs = $('<div/>').addClass('item')
             
             // verse reference
             .append( $('<div class="ref-and-first-words" />')
-                .append($('<h4/>').text(pv.ref))
-                .append($('<p/>').text( shortText )) 
+                .append($('<h4/>').text(vs.ref))
+                .append($('<p class="first-words" />').text( shortText   )) 
+                .append($('<p class="full-text"   />').text( vs.text     )) 
+                .append($('<p class="mnemonic"    />').text( vs.mnemonic )) 
+                .append($('<p class="mv-id"       />').text( vs.id       )) 
             )
             
             // add button
-            .append('<div class="select-verse-button"><a data-remote="true" href="/add/' + pv.id + '" class="compact-add-button" id="quick-start-add"></a></div>');
+            .append('<div class="select-verse-button"><a data-remote="true" href="/add/' + vs.id + '" class="compact-add-button"></a></div>');
 
-        $('.search-result-group').filter(':last').append($new_pv);                                                              
+        $('.search-result-group').filter(':last').append($new_vs);                                                              
     }); 
 
-
-    // if we have a single verse
-    
 }
 
