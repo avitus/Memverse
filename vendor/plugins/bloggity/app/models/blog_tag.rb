77949aec98c5e8aef998d821fc59bbba5b3c25a9
Andy <avitus@gmail.com>
2012-11-09 16:39:30 -0800

    Start moving blog to mountable engine

--- vendor/plugins/bloggity/app/models/blog_tag.rb
+++
@@ -1,8 +0,0 @@
-# t.string   "name"
-# t.integer  "blog_post_id"
-# t.datetime "created_at"
-# t.datetime "updated_at"
-			
-class BlogTag < ActiveRecord::Base
-	belongs_to :blog_post
-end
--- vendor/plugins/bloggity/init.rb
+++
@@ -1,9 +0,0 @@
-ActionView::Base.send :include, Bloggity::BloggityApplication
-ActionController::Base.send :include, Bloggity::BloggityApplication
-
-# FIX for engines model reloading issue in development mode -- ALV: commented out to support Rails 3
-#if ENV['RAILS_ENV'] != 'production'
-#	load_paths.each do |path|
-#		ActiveSupport::Dependencies.load_once_paths.delete(path)
-#	end
-#end
