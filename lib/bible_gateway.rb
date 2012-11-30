81b41fdb6f30e7153d7fcab0faca4687a51b9e4a
Alex Watt <alexcwatt@gmail.com>
2012-11-29 21:19:35 -0500

    No significant change to BibleGateway scrape_passage

--- lib/bible_gateway.rb
+++ lib/bible_gateway.rb
@@ -4,18 +4,18 @@ require 'nokogiri'
 
 
 class BibleGatewayError < StandardError; end
-  
+
 class BibleGateway
   GATEWAY_URL = "http://www.biblegateway.com"
-   
-  VERSIONS = { 
+
+  VERSIONS = {
     :NIV => "NIV1984",
     :NNV => "NIV",
-    :NAS => "NASB", 
-    :NKJ => "NKJV", 
+    :NAS => "NASB",
+    :NKJ => "NKJV",
     :KJV => "KJV",
     :RSV => "Revised Standard Version",
-    :NRS => "New Revised Standard Version",                          
+    :NRS => "New Revised Standard Version",
     :ESV => "ESV",
     :NLT => "NLT",
     :CEV => "CEV",
@@ -29,24 +29,24 @@ class BibleGateway
     :NVI => "Nueva Version Internacional",
     :RVR => "Reina-Valera 1960",
     :LSV => "Louis Segond 1910",
-    :LND => "La Nuova Diodati",    
+    :LND => "La Nuova Diodati",
     :AFR => "Afrikaans 1983 Translation",
     :HSV => "Herziene Statenvertaling",
     :NBV => "De Nieuwe Bijbelvertaling",
     :TMB => "Terjemahan Baru",
-    :SPB => "Svenska Folkbibeln" 
+    :SPB => "Svenska Folkbibeln"
   }
-  
+
   def self.versions
     VERSIONS.keys
   end
-  
+
   attr_accessor :version
-  
+
   def initialize(version = :NIV)
     self.version = version
   end
-  
+
   def version=(version)
     # raise BibleGatewayError, 'Unsupported version' unless VERSIONS.keys.include? version
     @version = version
@@ -54,13 +54,13 @@ class BibleGateway
 
   def lookup(passage)
   	if VERSIONS.keys.include?(version)
-		  doc = Nokogiri::HTML(open(passage_url(passage)))	  
+		  doc = Nokogiri::HTML(open(passage_url(passage)))
 		  scrape_passage(doc)
 		else
-		  {:title => "--", :content => "--" } 
-		end   	
+		  {:title => "--", :content => "--" }
+		end
   end
-  
+
   def passage_url(passage)
     URI.escape "#{GATEWAY_URL}/passage/?search=#{passage}&version=#{VERSIONS[version]}"
   end
@@ -71,17 +71,15 @@ class BibleGateway
     # segment = doc.at('font.woj') -- only works for words of Jesus
     # segment = doc.at('div.result-text-style-normal') -- old Biblegateway layout
     # Need to look for text immediately after superscript
-    
-      if !(doc.at('sup.versenum'))
-        #For when the verse is the first verse of the chapter
+
+      if !(doc.at('sup.versenum')) # if verse is first verse of chapter
         verse_number = doc.at('span.chapternum')
       else
-        #For all other cases
         verse_number = doc.at('sup.versenum')
       end
-      
+
     segment = verse_number.parent unless !verse_number
-  
+
  		#Untested fix for verses that have poetry in them
  		#if !(doc.at('div.poetry'))
  		  #For when the verse is not poetry
@@ -90,27 +88,21 @@ class BibleGateway
  		  #For when the verse is poetry
  		  #segment = verse_number.parent.parent unless !verse_number
  		#end
- 		        
+
     if segment
-	    
+
 	    segment.search("sup").remove                 # remove superscripts (verse numbering, footnotes, etc.)
 	    segment.search("h4").remove                  # remove headings
 	    segment.search("h5").remove                  # remove headings
 	    segment.search("div.footnotes").remove       # remove footnotes
 	    segment.search("div.crossrefs").remove       # remove cross references
-	    
-	    #Converts the special font that BibleGateway uses for 'LORD' and 'God' into 'LORD' and 'GOD'
+
+	    # Converts the special font that BibleGateway uses for 'LORD' and 'God' into 'LORD' and 'GOD'
 	    segment = segment.to_s.gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>", "LORD").gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">God</span>", "GOD")
 
-	    #Two different cases depending on if the verse is the first in the chapter or not
-	    if !(doc.at('sup.versenum'))
-	      #remove html tags, comments, non-breaking space, double spaces, trailing or leading whitespace, and chapter numbers
-        content = segment.gsub(/<b.+?<\/b>/," ").gsub(/<\/?[^>]*>/, " ").gsub(/<!--.*?-->/, " ").gsub("\u00A0", " ").gsub(/\s{2,}/, " ").strip
-        content = content.gsub(/^\d{1,3}/," ").strip
-	    else
-	      #remove html tags, comments, non-breaking space, double spaces, and trailing or leading whitespace
-        content = segment.gsub(/<b.+?<\/b>/," ").gsub(/<\/?[^>]*>/, " ").gsub(/<!--.*?-->/, " ").gsub("\u00A0", " ").gsub(/\s{2,}/, " ").strip
-	    end	    
+	    # remove html tags, comments, non-breaking space, double spaces, and trailing or leading whitespace
+      content = segment.gsub(/<b.+?<\/b>/," ").gsub(/<\/?[^>]*>/, " ").gsub(/<!--.*?-->/, " ").gsub("\u00A0", " ").gsub(/\s{2,}/, " ").strip
+      content = content.gsub(/^\d{1,3}/," ").strip # remove chapter numbers (if present)
     else
     	{:title => "--", :content => "--" }
     end
