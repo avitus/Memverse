8f6bb224f80b478107df24d14f5f8d76199737bf
Andy <avitus@gmail.com>
2012-12-03 17:04:53 -0800

    Fix for some broken tests

--- spec/models/passage_spec.rb
+++ spec/models/passage_spec.rb
@@ -26,40 +26,12 @@ describe Passage do
   describe "combining two passages" do
 
     before(:each) do
-
-      # Automatically generates user through Factory
+      # Automatically generates user, memverses and verses through Factory
       @psg1 = FactoryGirl.create(:passage, :book => 'Luke', :chapter => 2, :first_verse => 2, :last_verse => 4)
       @psg2 = FactoryGirl.create(:passage, :book => 'Luke', :chapter => 2, :first_verse => 5, :last_verse => 8)
-
-      @mvgrp1 = Array.new
-      @mvgrp2 = Array.new
-
-      for i in 2..4
-        verse   = FactoryGirl.create(:verse, :book_index => 42, :book => "Luke", :chapter => 2, :versenum => i, :text => "Christmas with angels and shepherds")
-        mv      = FactoryGirl.create(:memverse, :user => @user, :verse => verse, :passage => @psg1)
-        mv.passage = @psg1
-        mv.save
-        @mvgrp1 << mv
-      end
-
-      for i in 5..8
-        verse   = FactoryGirl.create(:verse, :book_index => 42, :book => "Luke", :chapter => 2, :versenum => i, :text => "Christmas with angels and shepherds")
-        mv      = FactoryGirl.create(:memverse, :user => @user, :verse => verse, :passage => @psg2)
-        mv.passage = @psg2
-        mv.save
-        @mvgrp2 << mv
-      end
-
     end
 
-    it "should merge two passages when the missing memverse is added" do
-
-      puts "In test: Passage 2 belongs to #{@psg2.user.name}"
-      puts "In test: mvgrp2 length is #{@mvgrp2.length}"
-      puts "In test: Passage 2 has #{@psg2.memverses.length} verses in it."
-      puts "In test: first element in mvgrp2: #{@mvgrp2.first.inspect}"
-      puts "In test: first element in mvgrp2 belongs to passage: #{@mvgrp2.first.passage.inspect}"
-      puts "In test: Passage 2: #{@psg2.inspect}"
+    it "should merge two adjacent passages" do
 
       expect {
         @psg1.combine_with( @psg2 )
@@ -67,13 +39,10 @@ describe Passage do
 
       @psg1.first_verse.should == 2
       @psg1.last_verse.should  == 8
+      @psg2.memverses.first.passage_id.should == @psg1.id # now associated with first passage
 
-      @mvgrp2.first.passage.id.should == @psg1.id  # Now associated with new passage
     end
 
   end
-
-
-
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  
+
   before(:each) do
-    @attr = { 
+    @attr = {
       :name => "Example User",
       :email => "user@example.com",
       :password => "foobar",
       :password_confirmation => "foobar"
     }
   end
-  
+
   it "should create a new instance given a valid attribute" do
     User.create!(@attr)
   end
-  
+
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-  
+
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
@@ -27,7 +27,7 @@ describe User do
       valid_email_user.should be_valid
     end
   end
-  
+
   it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
@@ -35,20 +35,20 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end
-  
+
   it "should reject duplicate email addresses" do
     User.create!(@attr)
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-  
+
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-  
+
   describe "passwords" do
 
     before(:each) do
@@ -63,7 +63,7 @@ describe User do
       @user.should respond_to(:password_confirmation)
     end
   end
-  
+
   describe "password validations" do
 
     it "should require a password" do
@@ -75,21 +75,21 @@ describe User do
       User.new(@attr.merge(:password_confirmation => "invalid")).
         should_not be_valid
     end
-    
+
     it "should reject short passwords" do
       short = "a" * 5
       hash = @attr.merge(:password => short, :password_confirmation => short)
       User.new(hash).should_not be_valid
     end
-    
+
   end
-  
+
   describe "password encryption" do
-    
+
     before(:each) do
       @user = User.create!(@attr)
     end
-    
+
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
@@ -99,37 +99,44 @@ describe User do
     end
 
   end
-  
+
+  # ==============================================================================================
+  # Check for complete chapters
+  # ==============================================================================================
   describe "complete chapters" do
+
     before(:each) do
       @user = FactoryGirl.create(:user)
       FactoryGirl.create(:final_verse, :book => "Psalms", :chapter => 117, :last_verse => 2)
       # TODO: Consider loading all FinalVerses anytime db is created (put in seeds file?)
     end
-	
+
     it "should list Psalm 117 when complete chapter is in account" do
       for i in 1..2
-        verse = FactoryGirl.create(:verse, :book_index => 19, :book => "Psalms", :chapter => '117', :versenum => i)
-        FactoryGirl.create(:memverse, :user_id => @user.id, :verse_id => verse.id)
+        vs = FactoryGirl.create(:verse, book: "Psalms", chapter: 117, versenum: i)
+        FactoryGirl.create(:memverse, user: @user, verse: vs)
       end
       @user.complete_chapters.first.should include("Psalm 117")
     end
-    
+
     it "should list also Psalm 117 when complete chapter, as well as verse 0, are in account" do
       for i in 0..2
-        verse = FactoryGirl.create(:verse, :book_index => 19, :book => "Psalms", :chapter => 117, :versenum => i)
-        FactoryGirl.create(:memverse, :user_id => @user.id, :verse_id => verse.id)
+        vs = FactoryGirl.create(:verse, book: "Psalms", chapter: 117, versenum: i)
+        FactoryGirl.create(:memverse, user: @user, verse: vs)
       end
       @user.complete_chapters.first.should include("Psalm 117")
     end
-    
+
     it "should not list Psalm 117 when only verse 2 is in account" do
-      verse = FactoryGirl.create(:verse, :book_index => 19, :book => "Psalms", :chapter => 117, :versenum => 2)
-      FactoryGirl.create(:memverse, :user_id => @user.id, :verse_id => verse.id)
+      vs = FactoryGirl.create(:verse, book: "Psalms", chapter: 117, versenum: 2)
+      FactoryGirl.create(:memverse, user: @user, verse: vs)
       @user.complete_chapters.first.should be_nil
     end
   end
 
+  # ==============================================================================================
+  # Adjusting work load
+  # ==============================================================================================
   describe "adjust_work_load" do
     it "should not change the account of an overworked user" do
       @user = FactoryGirl.create(:user, :time_allocation => 5)
@@ -141,7 +148,7 @@ describe User do
       @user.work_load.should == 5
       @user.adjust_work_load.should == false
     end
-    
+
     it "should adjust the work load of an underworked user" do
       @user = FactoryGirl.create(:user)
 
@@ -163,6 +170,9 @@ describe User do
     end
   end
 
+  # ==============================================================================================
+  # Reset memorization schedule
+  # ==============================================================================================
   describe "reset_memorization_schedule" do
     it "should space out verses appropriately" do
       @user = FactoryGirl.create(:user)
