1d54193dc6950b83bf6940749f09d8ae4ab0f2cd
Alex Watt <alexcwatt@gmail.com>
2012-12-08 07:25:43 -0500

    No change

--- lib/bible_gateway.rb
+++ lib/bible_gateway.rb
@@ -72,36 +72,36 @@ class BibleGateway
     # segment = doc.at('div.result-text-style-normal') -- old Biblegateway layout
     # Need to look for text immediately after superscript
 
-      if !(doc.at('sup.versenum')) # if verse is first verse of chapter
-        verse_number = doc.at('span.chapternum')
-      else
-        verse_number = doc.at('sup.versenum')
-      end
+    if !(doc.at('sup.versenum')) # if verse is first verse of chapter
+      verse_number = doc.at('span.chapternum')
+    else
+      verse_number = doc.at('sup.versenum')
+    end
 
- 		if !(doc.at('div.poetry'))
- 		  #For when the verse is not poetry
- 		  segment = verse_number.parent unless !verse_number
- 		else
- 		  #For when the verse is poetry
- 		  segment = verse_number.parent.parent unless !verse_number
- 		end
+    if !(doc.at('div.poetry'))
+      # For when the verse is not poetry
+      segment = verse_number.parent unless !verse_number
+    else
+      # For when the verse is poetry
+      segment = verse_number.parent.parent unless !verse_number
+    end
 
     if segment
 
-	    segment.search("sup").remove                 # remove superscripts (verse numbering, footnotes, etc.)
-	    segment.search("h4").remove                  # remove headings
-	    segment.search("h5").remove                  # remove headings
-	    segment.search("div.footnotes").remove       # remove footnotes
-	    segment.search("div.crossrefs").remove       # remove cross references
+      segment.search("sup").remove                 # remove superscripts (verse numbering, footnotes, etc.)
+      segment.search("h4").remove                  # remove headings
+      segment.search("h5").remove                  # remove headings
+      segment.search("div.footnotes").remove       # remove footnotes
+      segment.search("div.crossrefs").remove       # remove cross references
 
-	    # Converts the special font that BibleGateway uses for 'LORD' and 'God' into 'LORD' and 'GOD'
-	    segment = segment.to_s.gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>", "LORD").gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">God</span>", "GOD")
+      # Converts the special font that BibleGateway uses for 'LORD' and 'God' into 'LORD' and 'GOD'
+      segment = segment.to_s.gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>", "LORD").gsub("<span style=\"font-variant: small-caps\" class=\"small-caps\">God</span>", "GOD")
 
-	    # remove html tags, comments, non-breaking space, double spaces, and trailing or leading whitespace
+      # remove html tags, comments, non-breaking space, double spaces, and trailing or leading whitespace
       content = segment.gsub(/<b.+?<\/b>/," ").gsub(/<\/?[^>]*>/, " ").gsub(/<!--.*?-->/, " ").gsub("\u00A0", " ").gsub(/\s{2,}/, " ").strip
       content = content.gsub(/^\d{1,3}/," ").strip # remove chapter numbers (if present)
     else
-    	{:title => "--", :content => "--" }
+      {:title => "--", :content => "--" }
     end
     {:title => title, :content => content }
   end
