4cf0c0371cabea808e2c0d38465b4591fc2a650e
Andy <avitus@gmail.com>
2013-01-29 18:21:21 -0800

    Trying to get all tests passing

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,59 +1,59 @@
 class BadgesController < ApplicationController
-  
+
   before_filter :authenticate_user!
-  
+
   def index
   end
 
   def show
   end
-  
-  # ----------------------------------------------------------------------------------------------------------   
+
+  # ----------------------------------------------------------------------------------------------------------
   # Show badges earned by user
-  # ----------------------------------------------------------------------------------------------------------   
+  # ----------------------------------------------------------------------------------------------------------
   def earned_badges
-    
+
     user = User.find(params[:id])
     @earned_badges = user.badges unless !user
 
     respond_to do |format|
       format.html  # earned_badges.html.erb
       format.json  { render :json => @earned_badges }
-    end    
-    
+    end
+
   end
 
-  # ----------------------------------------------------------------------------------------------------------   
+  # ----------------------------------------------------------------------------------------------------------
   # Check whether user has earned any badges
-  # ----------------------------------------------------------------------------------------------------------  
+  # ----------------------------------------------------------------------------------------------------------
   def badge_completion_check
-    
+
     @recently_awarded_badges = Array.new
-    
+
     # Get badges user is working towards. Check most valuable (gold) badges first
     badges_to_check = current_user.badges_to_strive_for.sort.reverse
-        
+
     badges_to_check.each do |badge|
-      Rails.logger.info("Checking whether user has earned #{badge.color} #{badge.name} badge")
+
       if badge.achieved?(current_user)
         badge.award_badge(current_user)
         @recently_awarded_badges << badge
-        
+
         if badge.color == "solo"
           broadcast = "#{current_user.name_or_login} has been awarded the #{badge.name} badge"
         else
           broadcast  = "#{current_user.name_or_login} has been awarded a #{badge.color} #{badge.name} badge"
-        end       
-        Tweet.create(:news => broadcast, :user_id => current_user.id, :importance => 2)        
-        
+        end
+        Tweet.create(:news => broadcast, :user_id => current_user.id, :importance => 2)
+
       end
-    end  
-      
+    end
+
     respond_to do |format|
-      format.html  
+      format.html
       format.json  { render :json => @recently_awarded_badges }
-    end        
-      
-  end    
-    
+    end
+
+  end
+
 end
--- features/badges/earn_consistency_badge.feature
+++ features/badges/earn_consistency_badge.feature
@@ -2,18 +2,18 @@ Feature: Earn badges
   In order to have a feeling of achievement
   A user
   Should be able to earn badges
-  
+
   Background:
   	Given I sign in as an advanced user
-     
-    @javascript
+
+    @javascript @badges
     Scenario: User completes final session required to earn silver consistency badge
       Given the user with the email of "advanced_user@test.com" has completed 324 memorization sessions in the past year
       When the user with the email of "advanced_user@test.com" completes a memorization session
       Then I should see "CONGRATULATIONS"
+      When I go to advanced's dashboard
       When I go to the home page
-      When I follow "Dashboard"
       Then I should see "advanced has been awarded a silver Consistency badge"
       When I go to the progress page
       Then I should not see "CONGRATULATIONS"
-      
+
