e81ee75889e561eff5a8a21b5ecf8a8c894f9b48
Josiah DeGraaf <josiahdegraaf@gmail.com>
2013-03-19 17:11:26 -0400

    Current Progress on Consistency Chart

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -58,6 +58,48 @@ class ChartController < ApplicationController
     end 
   end   
 
+  # ----------------------------------------------------------------------------------------------------------
+  # Chart Showing User Consistency
+  # ----------------------------------------------------------------------------------------------------------
+  def load_consistency_progress
+
+    chart_data = Array.new    
+    # Set up data series
+    y_consistency       = Array.new
+    x_consistency_date  = Array.new    
+    
+    # Get progress entries for specified user or current user
+    if params[:user]
+      user_id = params[:user]
+    else
+      user_id = current_user.id
+    end
+
+    all_entries = ProgressReport.find( :all, :conditions => { :user_id => user_id }) 
+    
+    # TODO: Consider deleting extra entries periodically
+    # TODO: Add in last entry so that graph always shows current day
+    entries = all_entries.length > 160 ? all_entries.every( all_entries.length / 80 ) : all_entries
+    
+    if !entries.empty?
+      # Build data series
+      entries.each { |entry|
+        y_consistency           << entry.completed_sessions
+        x_consistency_date      << entry.entry_date.to_s
+      }
+    else
+        y_consistency      << 0
+        x_consistency_date << 0
+    end
+               
+    chart_data << y_consistency
+    chart_data << x_consistency_date
+    
+    respond_to do |format|
+      format.json { render json: chart_data }
+    end 
+  end 
+
 
   # ----------------------------------------------------------------------------------------------------------
   # Chart Showing Total Users and Memory Verses
