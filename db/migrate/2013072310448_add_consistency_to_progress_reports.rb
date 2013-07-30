2bec0007c90306967a23a2c99f50e858e356b1a8
Josiah DeGraaf <josiahdegraaf@gmail.com>
2013-07-29 23:25:02 -0400

    Consistency Chart

---
+++ db/migrate/2013072310448_add_consistency_to_progress_reports.rb
@@ -0,0 +1,7 @@
+class AddConsistencyToProgressReports < ActiveRecord::Migration
+  def up
+    add_column :progress_reports, :consistency, :integer
+    ProgressReport.reset_column_information
+    ProgressReport.all.each {|pr| pr.save! }
+  end
+end
