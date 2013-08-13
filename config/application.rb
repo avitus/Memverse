927f4c7461e2b85f0964a0ffc1b45039266a0cad
Andy <avitus@gmail.com>
2013-08-13 14:10:43 -0700

    Fix more tests

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'rails/all'
+
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
+
+# These five frameworks were included under Rails 3.2
+# require "active_record/railtie"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "active_resource/railtie"
+# require "sprockets/railtie"
+
+# This framework was not required as of Rails 3.2
 # require "rails/test_unit/railtie"
 
-# If you have a Gemfile, require the default gems, the ones in the
-# current environment and also include :assets gems if in development
-# or test environments.
-
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module MemverseApp
@@ -58,5 +62,9 @@ module MemverseApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't require attr_accessible to be defined for every model
+    config.active_record.whitelist_attributes = false
+
   end
 end
