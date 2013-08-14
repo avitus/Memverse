d392e983fa296f79a4d7e34509bbdafb57c18c23
Andy <avitus@gmail.com>
2013-08-14 14:11:47 -0700

    All rspec tests passing

--- spec/models/passage_spec.rb
+++ spec/models/passage_spec.rb
@@ -37,19 +37,7 @@ describe Passage do
     vs = FactoryGirl.create(:verse, book: 'Nahum', chapter: 1, versenum: 6)
     mv = FactoryGirl.create(:memverse_without_supermemo_init, :verse => vs, :efactor => 1.4, :rep_n => 2, :test_interval => 3)
 
-    # The problem here is that the factory for the Passage model is not creating the associated Memverses
-    pp psg
-    pp psg.memverses
-    pp psg.memverses.active.first
-    pp psg.memverses.active.last
-
     psg.expand( mv )
-
-    pp psg
-
-    pp psg.memverses.active[0]
-    pp psg.memverses.active[1]
-    pp psg.memverses.active[2]
 
     psg.test_interval.should == 3
     psg.rep_n.should         == 2
@@ -81,17 +69,22 @@ describe Passage do
     it "should merge two passages if linking verse is inserted" do
 
       # Automatically generates user, memverses and verses through Factory
-      psg1 = FactoryGirl.create(:passage, user: @user, book: 'Mark', chapter: 2, first_verse: 2, last_verse: 4)
-      psg2 = FactoryGirl.create(:passage, user: @user, book: 'Mark', chapter: 2, first_verse: 6, last_verse: 8)
+
+      # Mark 2:2-4
+      psg1 = FactoryGirl.create(:passage, user: @user, book: 'Mark', chapter: 2, first_verse: 2, last_verse: 4, length: 3)
+      # Mark 2:6-8
+      psg2 = FactoryGirl.create(:passage, user: @user, book: 'Mark', chapter: 2, first_verse: 6, last_verse: 8, length: 3)
 
       mv2 = psg2.memverses.first # need to ensure that this verse is associated with psg1 after combination
 
+      # Mark 2:5
       vs = FactoryGirl.create(:verse, book: 'Mark', chapter: 2, versenum: 5)
       mv = FactoryGirl.create(:memverse, user: @user, verse: vs)  # triggers after_create call_back to add memory verse to passage
 
       psg1.reload
       mv2.reload
 
+      # Assertions
       psg1.first_verse.should == 2
       psg1.last_verse.should  == 8
       psg1.length.should      == 7
