e4b8dce16d432c56a529ee3a8bce79378018a679
Andy <avitus@gmail.com>
2013-08-21 20:23:28 -0700

    Update bundle, consistency report tests failing

--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -1,24 +1,25 @@
 class ProgressReport < ActiveRecord::Base
 
-  # Structure
+  #  Structure
   #  t.integer   :user_id,     :null => false
   #  t.date      :entry_date
   #  t.integer   :learning      
   #  t.integer   :memorized
   #  t.integer   :time_allocation
-
+  #  t.integer   :consistency
 
   # Relationships
   belongs_to  :user
   
   # Validations
   validates_presence_of :user_id, :learning, :memorized, :entry_date
-  before_save :setup_consistency 
+  before_save :capture_consistency 
 
   protected
   
-  def setup_consistency
-    self.consistency = ProgressReport.where(:user_id => user_id).where('entry_date > ? and entry_date < ?', entry_date - 1.year, entry_date + 1).count
+  def capture_consistency
+    # self.consistency = ProgressReport.where(:user_id => user_id).where('entry_date > ? and entry_date < ?', entry_date - 1.year, entry_date + 1).count
+    self.consistency = ProgressReport.where(:user_id => user_id).where(:entry_date => (entry_date - 1.year)..(entry_date + 1)).count
   end
   
 end
