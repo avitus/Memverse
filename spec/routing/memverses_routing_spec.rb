8ee98e364c2a38d04c1e113d86514f6cf01cee2e
Andy <avitus@gmail.com>
2013-08-26 15:44:14 -0700

    Add routes for Memverses controller

---
+++ spec/routing/memverses_routing_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Routing to memverses" do
+
+  it "route to add a chapter" do
+	expect(:post => "/add_chapter").to route_to(
+	  :controller => "memverses",
+	  :action     => "add_chapter"
+	)
+  end
+
+  it "route to add a tag" do
+	expect(:post => "/add_tag").to route_to(
+	  :controller => "memverses",
+	  :action     => "add_mv_tag"
+	)
+  end
+
+  it "route to autocomplete tag" do
+	expect(:get => "/tag_autocomplete").to route_to(
+	  :controller => "memverses",
+	  :action     => "tag_autocomplete"
+	)
+  end
+
+end
