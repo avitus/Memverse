5327b95b007cb00fedd9f6c89d3a1fc3f12935b0
Andy <avitus@gmail.com>
2013-08-27 16:00:00 -0700

    Index verse model for search

---
+++ app/indices/verse_index.rb
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------------------------------------------
+# Sphinx Index
+# ----------------------------------------------------------------------------------------------------------
+
+ThinkingSphinx::Index.define :verse, :with => :active_record do
+    # fields
+    indexes translation, :sortable => true
+    indexes text
+    indexes [book, chapter, versenum], :as => :reference
+
+    # attributes
+    has created_at
+end
