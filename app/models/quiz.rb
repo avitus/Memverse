57c53f3476a39a3dcb29a42961c46e47f227d3ee
Andy <avitus@gmail.com>
2013-09-21 20:42:53 -0700

    Moving quiz functions to separate file

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -13,21 +13,29 @@ class Quiz < ActiveRecord::Base
   # validates_presence_of :user_id
 
   def update_length
+    
     length = 0
 
     for question in self.quiz_questions
-      case question.question_type
-      when "recitation"
-        length = length + (question.passage_translations.first.last.split(" ").length * 2.5 + 15.0).to_i + 1 # 24 WPM typing speed with 15 seconds to think
-      when "reference"
-        length = length + 25 + 1
-      when "mcq"
-        length = length + 30 + 1
-      end
+      
       # Note that 1 is added to each of these because we put a 1 second gap between questions
+      case question.question_type
+      
+        when "recitation"
+          length = length + (question.passage_translations.first.last.split(" ").length * 2.5 + 15.0).to_i + 1 # 24 WPM typing speed with 15 seconds to think
+      
+        when "reference"
+          length = length + 25 + 1
+      
+        when "mcq"
+          length = length + 30 + 1
+        end
+    
     end
-    self.quiz_length = length
+  
+  self.quiz_length = length
 	self.save
+  
   end
 
   # ============= Protected below this line ==================================================================
