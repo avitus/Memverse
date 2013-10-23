5dca8294a98ffb46b309bafa505970e08de3cd10
Andy <avitus@gmail.com>
2013-10-22 17:07:04 -0700

    MIGRATION - add fields to quiz question

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,10 @@
+# -----------------------------------------------------------------------------------------------------
+# ALV - would like to use LocaleApp for translations but need repo to be public. Before this can happen
+#       we need to remove all passwords from the app.
+# -----------------------------------------------------------------------------------------------------
+
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key = 'ej5tKKSI8XcE1CiGVeypK4hE817lCNtmZeCQmlLWE4rJhJKco3'
+  config.api_key = 'hydwsckJ8pQAuHIcG9FNcVFm8np2mrtIKdiKUP84sv1ex4hfrk'
 end
--- db/migrate/20130919050936_add_details_to_quiz_questions.rb
+++ db/migrate/20130919050936_add_details_to_quiz_questions.rb
@@ -1,13 +1,14 @@
 class AddDetailsToQuizQuestions < ActiveRecord::Migration
   def change
-    
+
     add_column :quiz_questions, :times_answered, :integer, :default => 0
     add_column :quiz_questions, :perc_correct,   :decimal, :default => 50  # default to medium difficulty
     add_column :quiz_questions, :mcq_category,   :string,  index: true
+    add_column :quiz_questions, :last_asked,     :date,    :default => Date.today - 1.year
 
     # Foreign key for Uberverse model
     add_column :quiz_questions, :supporting_ref, :integer, index: true
-    
+
     # Foreign key for User model
     add_column :quiz_questions, :submitted_by,   :integer, index: true
 
