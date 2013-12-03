0d7bc206d763996c16dfc893fb3c8579c2b7b886
Andy <avitus@gmail.com>
2013-12-02 17:08:21 -0800

    Capture users who create account using Oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def windowslive
 
-      @user = User.find_for_windowslive_oauth2(request.env["omniauth.auth"], current_user)
+      @user = User.find_for_windowslive_oauth2( request.env["omniauth.auth"], current_user )
 
-      #if @user.persisted?
-      #  flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "WindowsLive"
-      #  sign_in_and_redirect @user, :event => :authentication
-      #else
-      #  session["devise.windowslive_data"] = request.env["omniauth.auth"]
-      #  redirect_to new_user_registration_path
-      #end
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "WindowsLive"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.windowslive_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_path
+      end
+
   end
 end
