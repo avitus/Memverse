2fe22b0aee5d307d7001b8c79e44b58f62469403
Andy <avitus@gmail.com>
2013-12-10 13:13:21 -0800

    Specs for ref and accuracy score

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,25 +1,67 @@
 require 'spec_helper'
 
 describe UsersController do
-  
-  before (:each) do
-    @user = FactoryGirl.create(:user)
-    @user.confirm!
-    sign_in @user
+
+  before(:each) do
+    login_user
   end
 
   describe "GET 'show'" do
-    
+
     it "should be successful" do
       get :show, :id => @user.id
       response.should be_success
     end
-    
+
     it "should find the right user" do
       get :show, :id => @user.id
       assigns(:user).should == @user
     end
-    
+
+  end
+
+  # ==============================================================================================
+  # Updating reference score
+  # ==============================================================================================
+  describe "update_ref_grade" do
+
+    context 'when logged in' do
+      it "should save updated reference score" do
+        post :update_ref_grade, :score => 75, :format => :json
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when logged out' do
+      it "should redirect user" do
+        sign_out @user
+        post :update_ref_grade, :score => 75, :format => :json
+        expect(response.status).to eq(401)
+      end
+    end
+
+  end
+
+  # ==============================================================================================
+  # Updating accuracy score
+  # ==============================================================================================
+  describe "update_accuracy" do
+
+    context 'when logged in' do
+      it "should save updated accuracy score" do
+        post :update_accuracy, :score => 75, :format => :json
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when logged out' do
+      it "should redirect user" do
+        sign_out @user
+        post :update_accuracy, :score => 75, :format => :json
+        expect(response.status).to eq(401)
+      end
+    end
+
   end
 
 end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,17 @@
+module ControllerMacros
+
+  puts " ====> Loading controller macros."
+
+  def login_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = FactoryGirl.create(:user)
+    @user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
+    sign_in @user
+  end
+
+  def login_admin
+    @request.env["devise.mapping"] = Devise.mappings[:admin]
+    sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+  end
+
+end
