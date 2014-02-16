94dbb29ddc42956712cd5b323ad9348b749098b6
Alex Watt <alexcwatt@gmail.com>
2014-02-16 15:44:43 -0500

    Failing spec: Unabbreviate accepts non-standard capitalization, #210

--- spec/javascripts/unabbreviate_spec.js
+++ spec/javascripts/unabbreviate_spec.js
@@ -2,11 +2,19 @@ describe("Unabbreviate", function() {
   it("handles standard abbreviations", function() {
     expect(unabbreviate("Jn")).toEqual("John");
   });
+
   it("handles non-standard abbreviations", function() {
     expect(unabbreviate("Joh")).toEqual("John");
     expect(unabbreviate("Mat")).toEqual("Matthew");
   });
+
+  it("accepts non-standard capitalization", function(){
+    expect(unabbreviate("JOHN")).toEqual("John");
+    expect(unabbreviate("gEnE")).toEqual("Genesis");
+  });
+
   it("fails when too general", function() {
     expect(unabbreviate("M")).toEqual("M");
   });
+
 });
