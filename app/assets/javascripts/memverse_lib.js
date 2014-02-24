avitus/Memverse

commit 22613f45ae81ffa1da7c6998bc47cc79ca4a4a01

Passing spec: Unabbreviate accepts non-standard capitalization, fixes #210

app/assets/javascripts/memverse_lib.js
@@ -131,6 +131,9 @@ String.prototype.capitalize = function() {
  * Substitute abbreviations
  ******************************************************************************/
 function unabbreviate(book_name) {
+	// capitalize only first letter of each word of book_name
+	book_name = book_name.toLowerCase().capitalize();
+
 	if(!(book_name.split(" ")[0].match('[^I]'))) { // Check if first "word" contains only I's; then Roman numerals to Arabic numbers
 		book_name = book_name.replace("III ", "3 ").replace("II ", "2 ").replace("I ", "1 "); // replace first occurences
 	}
@@ -314,7 +317,7 @@ function parseVerseRef(verseref) {
 
 		vs = parseInt(split_text.pop());
 		ch = parseInt(split_text.pop());
-		bk = unabbreviate( split_text.join(' ').capitalize() );
+		bk = unabbreviate( split_text.join(' ') );
 		bi = jQuery.inArray( bk, BIBLEBOOKS );
 
 		if (bi === -1) {
@@ -371,7 +374,7 @@ function parsePassageRef(passage) {
 		}
 
 		ch       = parseInt(split_text.pop());
-		bk       = unabbreviate( split_text.join(' ').capitalize() );
+		bk       = unabbreviate( split_text.join(' ') );
 		bi       = jQuery.inArray( bk, BIBLEBOOKS );
 
 		if (bi === -1) {

commit 17ddcd26417cb6456e2e85059b39e8e0db38061a

Song Of Songs fix. Caused by 22613f45ae81ffa1da7c6998bc47cc79ca4a4a01

app/assets/javascripts/memverse_lib.js
@@ -310,7 +310,7 @@ function parseVerseRef(verseref) {
 	if (validVerseRef(verseref)) {
 
 		// Handle corner cases
-		verseref = verseref.replace(/(song of songs)/i, "Song of Songs")
+		verseref = verseref.replace(/(song of songs)/i, "Song Of Songs")
 						   .replace(/(psalm )/i,        "Psalms ");
 
 		split_text = verseref.split(/:\s|:|\s/);
@@ -361,7 +361,7 @@ function parsePassageRef(passage) {
 	if (validPassageRef(passage)) {
 
 		// Handle corner cases
-		passage = passage.replace(/(song of songs)/i, "Song of Songs")
+		passage = passage.replace(/(song of songs)/i, "Song Of Songs")
 						 .replace(/(psalm )/i,        "Psalms ")
                          .replace(/\:\s/, ':') // change colon + space to just a colon
 						 .replace(/\:$/, '');  // Something like "Romans 8:" -- can happen on Add Verse page as user types