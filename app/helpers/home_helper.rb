c62a79a4ae98e5c6c2fd4719f9c424d11ac30007
Alex Watt <alexcwatt@gmail.com>
2014-03-14 23:39:31 -0400

    Translations, not versions

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,12 @@
 module HomeHelper
 
-  # Public: Link for setting Bible version on quick_start
+  # Public: Link for setting Bible translation on quick_start
   #
-  # abbrev - The String for the abbreviation used to identify the version.
-  # name   - The String for the name of the version displayed to the user.
+  # abbrev - The String for the abbreviation used to identify the translation.
+  # name   - The String for the name of the translation displayed to the user.
   #
   # Returns the HTML String.
-  def version_set_link(abbrev, name)
+  def trans_set_link(abbrev, name)
     content_tag(:li, id: abbrev, class: "tl-set") do
       link_to name, set_translation_path(abbrev), remote: true
     end
