ccbe26ff1969b3511422b83a8ddeb8f94574d77a
Andy <avitus@gmail.com>
2014-04-30 10:33:00 -0700

    Fewer forems on each page

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,7 +1,7 @@
 Forem.user_class         = "User"
 Forem.email_from_address = "admin@memverse.com"
 Forem.avatar_user_method = :blog_avatar_url  # ALV: match blog avatar
-Forem.per_page           = 25
+Forem.per_page           = 20
 Forem.user_profile_links = true
 Forem.layout             = 'application'
 
