d760574d4c89f1c7bb02f7400e0e3a7f96d04a22
Andy <avitus@gmail.com>
2014-06-05 13:39:08 -0700

    Working on Spritz reading

---
+++ app/controllers/reading_controller.rb
@@ -0,0 +1,14 @@
+class ReadingController < ApplicationController
+
+  def chapter
+
+    @chapter_text = Verse.where(:book => params[:bk], :chapter => params[:ch], :translation => "NIV").order(:versenum).pluck(:text).join(" ")
+
+    respond_to do |format|
+      format.html { render text: @chapter_text }
+      format.json { render json:  @chapter_text }
+    end
+
+  end
+
+end
