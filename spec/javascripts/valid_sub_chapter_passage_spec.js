db5ccad11bca95c41c5abc9d4e1010a8065ae2f1
Alex Watt <alexcwatt@gmail.com>
2014-06-24 19:40:08 -0400

    Failing spec for #264 (Korean support)

--- spec/javascripts/valid_sub_chapter_passage_spec.js
+++ spec/javascripts/valid_sub_chapter_passage_spec.js
@@ -2,11 +2,11 @@ describe("validSubChapterPassage", function() {
   it("rejects single verse references", function() {
     expect(validSubChapterPassage("Genesis 1:27")).toEqual( false );
   });
-  
+
   it("accepts chapters", function() {
     expect(validSubChapterPassage("Genesis 1")).toEqual( false );
   });
-  
+
   it("accepts passages", function() {
     expect(validSubChapterPassage("Genesis 1:1-5")).toEqual( true );
   });
