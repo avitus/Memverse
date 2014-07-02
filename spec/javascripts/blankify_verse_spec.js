02adbef2c3a32d605e901ea808a229d3af962b6d
Alex Watt <alexcwatt@gmail.com>
2014-07-02 13:13:00 -0400

    Working on #255

--- spec/javascripts/blankify_verse_spec.js
+++ spec/javascripts/blankify_verse_spec.js
@@ -6,17 +6,17 @@ describe("blankifyVerse", function() {
 
   it("replaces 10% of words in a verse with blanks", function() {
     expect(blankifyVerse("In the beginning God created the heavens and the earth", 10))
-    .toEqual("<span>In </span> <span>the </span> <input name='beginning' class='blank-word' style='width:78px'> <span>God </span> <span>created </span> <span>the </span> <span>heavens </span> <span>and </span> <span>the </span> <span>earth </span>");
+    .toEqual("<span>In </span> <span>the </span> <input name='beginning' class='blank-word' style='width:85px'> <span>God </span> <span>created </span> <span>the </span> <span>heavens </span> <span>and </span> <span>the </span> <span>earth </span>");
   });
 
   it("replaces 40% of words in a verse with blanks", function() {
     expect(blankifyVerse("In the beginning God created the heavens and the earth", 40))
-    .toEqual("<span>In </span> <span>the </span> <input name='beginning' class='blank-word' style='width:78px'> <span>God </span> <input name='created' class='blank-word' style='width:53px'> <span>the </span> <input name='heavens' class='blank-word' style='width:61px'> <span>and </span> <span>the </span> <input name='earth' class='blank-word' style='width:37px'>");
+    .toEqual("<span>In </span> <span>the </span> <input name='beginning' class='blank-word' style='width:85px'> <span>God </span> <input name='created' class='blank-word' style='width:65px'> <span>the </span> <input name='heavens' class='blank-word' style='width:75px'> <span>and </span> <span>the </span> <input name='earth' class='blank-word' style='width:44px'>");
   });
 
   it("replaces plain apostrophes with fancy ones", function() {
     expect(blankifyVerse("for his name's sake", 40))
-    .toEqual("<span>for </span> <span>his </span> <input name='name’s' class='blank-word' style='width:51px'> <input name='sake' class='blank-word' style='width:33px'>");
+    .toEqual("<span>for </span> <span>his </span> <input name='name’s' class='blank-word' style='width:63px'> <input name='sake' class='blank-word' style='width:42px'>");
   });
 
 });
