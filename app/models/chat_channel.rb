6107f938cb8e4e61def07d5d7482841146123d3c
Alex Watt <alexcwatt@gmail.com>
2014-07-15 15:04:47 -0400

    DRY ChatChannel

--- app/models/chat_channel.rb
+++ app/models/chat_channel.rb
@@ -2,15 +2,6 @@ class ChatChannel
 
   attr_reader :channel
 
-  # PubNub callback
-  PN_callback = lambda { |envelope|
-    if envelope.error
-      # If message is not sent we should probably try to send it again
-      puts("==== ! Failed to send message ! ==========")
-      puts( envelope.inspect )
-    end
-  }
-
   def initialize(channel)
     @channel = channel
   end
@@ -26,15 +17,7 @@ class ChatChannel
   def status=(str)
     $redis.hset("chat-#{channel}", "status", str)
 
-    PN.publish(
-      channel: channel,
-      message: {
-        meta: "chat_status",
-        status: str
-      },
-      http_sync: true,
-      callback: PN_callback
-    )
+    publish(meta: "chat_status", status: str)
   end
 
   def open?
@@ -49,18 +32,28 @@ class ChatChannel
     self.status = open? ? "Closed":"Open"
   end
 
+
+  def publish(msg)
+    callback = lambda { |envelope|
+      if envelope.error
+        # If message is not sent we should probably try to send it again
+        puts("==== ! Failed to send message ! ==========")
+        puts( envelope.inspect )
+      end
+    }
+
+    PN.publish(
+      channel: channel,
+      message: msg,
+      http_sync: true,
+      callback: callback
+    )
+  end
+
   def send_message(msg)
     if open?
       Rails.logger.info("====> Publishing message to PubNub: #{msg}")
-      PN.publish(
-        channel: channel,
-        message: {
-          meta: "chat",
-          data: msg
-        },
-        http_sync: true,
-        callback: PN_callback
-      )
+      publish(meta: "chat", data: msg)
     else
       puts "Could not send message. Channel #{channel} closed."
       return false
