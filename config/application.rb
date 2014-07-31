1b7a6cc0a3fd3a6186cc7fdc349d7a8ea012d3c3
Alex Watt <alexcwatt@gmail.com>
2014-07-31 19:03:48 -0400

    Fallback on English if translation missing

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,10 @@ module MemverseApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Fallback on en if translation missing
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
