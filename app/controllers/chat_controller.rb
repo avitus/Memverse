612e988013caff4743a0d784c377c011c2b24ed5
Alex Watt <alexcwatt@gmail.com>
2014-08-02 19:45:10 -0400

    Quiz managers can ban chat users

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -20,7 +20,7 @@ class ChatController < ApplicationController
   def toggle_ban
     user_id = params[:user_id]
 
-    if user_id && current_user.admin?
+    if user_id && can? :manage, Quiza
       if $redis.exists("banned-#{user_id}")
         $redis.del("banned-#{user_id}")
         status = "Ban revoked"
