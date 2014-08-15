307598e3435a0ee671376f3dc63a5b628b1e7902
Alex Watt <alexcwatt@gmail.com>
2014-08-15 09:14:34 -0400

    Fix CountryController#show (#289)

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -1,12 +1,12 @@
 class CountryController < ApplicationController
- 
+
   add_breadcrumb "Home", :root_path
   add_breadcrumb "Country Leaderboard", :countryboard_path
 
   def show
-    @country  = Country.find(params[:id])
+    @country  = Country.friendly.find(params[:id])
     @users    = @country.users.active.order('memorized DESC')
     add_breadcrumb @country.printable_name, {:action => "show", :id => params[:id]}
-  end   
-  
+  end
+
 end
