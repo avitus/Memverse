3c41d08f9f7e7c3bec5d1f851e758df5880099fc
Alex Watt <alexcwatt@gmail.com>
2014-12-24 13:19:07 -0500

    Refactor User; YARD for User and ProgressReport

---
+++ app/models/bloggity_user.rb
@@ -0,0 +1,64 @@
+# Bloggity settings
+module BloggityUser
+  # Display name
+  #
+  # @return [String]
+  def display_name
+    self.name || self.login
+  end
+
+  # Can user post to given blog?
+  #
+  # @param blog_id [Fixnum, nil]
+  # @return [Boolean]
+  def can_blog?(blog_id = nil)
+    self.has_role?("blogger")
+  end
+
+  # Can user moderate the comments for a given blog?
+  #
+  # @param blog_id [Fixnum, nil]
+  # @return [Boolean]
+  def can_moderate_blog_comments?(blog_id = nil)
+    self.admin?
+  end
+
+  # Are user's comments on given blog automatically approved?
+  # If not, they will be queued until a moderator approves them
+  #
+  # @param blog_id [Fixnum, nil]
+  # @return [Boolean]
+  def blog_comment_auto_approved?(blog_id = nil)
+    self.memverses.learning.count > 10  or self.completed_sessions >= 5
+  end
+
+  # Can user comment on given blog?
+  #
+  # @param blog_id [Fixnum, nil]
+  # @return [Boolean]
+  def can_comment?(blog_id = nil)
+  	self.completed_sessions >= 2 # We need this to control the spammers
+  end
+
+  # Can user create, edit and destroy blogs?
+  #
+  # @return [Boolean]
+  def can_modify_blogs?
+    self.admin?
+  end
+
+  def user_signed_in?
+    true
+  end
+
+  # Path to user's avatar, using Gravatar
+  def blog_avatar_url
+    if(self.respond_to?(:email))
+      downcased_email_address = self.email.downcase
+      hash = Digest::MD5::hexdigest(downcased_email_address)
+      "https://www.gravatar.com/avatar/#{hash}?d=wavatar"
+    else
+      "https://www.pistonsforum.com/images/avatars/avatar22.gif"
+    end
+  end
+end
--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -17,9 +17,13 @@ class ProgressReport < ActiveRecord::Base
 
   protected
   
+  # Compute consistency [trigger: before_save]
+  # This counts the number of sessions the user has completed over the past year.
+  # Max of 366 (leap year).
+  # @return [void]
   def capture_consistency
     # Need to add 1 to consistency count because current progress report hasn't yet been saved
-    self.consistency = ProgressReport.where(:user_id => user_id).where(:entry_date => (entry_date - 1.year + 1)..entry_date).count + 1
+    self.consistency = user.progress_reports.where(entry_date: (entry_date - 1.year + 1)..entry_date).count + 1
   end
   
 end
