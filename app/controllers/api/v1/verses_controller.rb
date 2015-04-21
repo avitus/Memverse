b78b644572ffb22fcf55ef8de6785102fc1a4400
Andy <avitus@gmail.com>
2015-04-20 17:59:22 -0700

    More work on API documentation

--- app/controllers/api/v1/verses_controller.rb
+++ app/controllers/api/v1/verses_controller.rb
@@ -40,10 +40,71 @@ class Api::V1::VersesController < Api::V1::ApiController
     end
   end
 
+
+  swagger_path '/verses/lookup' do
+    operation :get do
+      key :description, 'Lookup a verse by translation, book, chapter, and verse number'
+      key :operationId, 'findVerseByTlBkChVs'
+      key :tags, ['verse']
+
+      parameter do
+        key :name, :tl
+        key :in, :query
+        key :description, 'Bible translation of required verse'
+        key :required, true
+        key :type, :string
+      end
+
+      parameter do
+        key :name, :bk
+        key :in, :query
+        key :description, 'Book'
+        key :required, true
+        key :type, :string
+      end
+
+      parameter do
+        key :name, :ch
+        key :in, :query
+        key :description, 'Chapter'
+        key :required, true
+        key :type, :string
+      end
+
+      parameter do
+        key :name, :vs
+        key :in, :query
+        key :description, 'Verse number'
+        key :required, true
+        key :type, :string
+      end
+
+
+      response 200 do
+        key :description, 'verse response'
+        schema do
+          key :'$ref', :Verse
+        end
+      end
+      response 401 do
+        key :description, 'unauthorized response'
+        schema do
+          key :'$ref', :Verse
+        end
+      end
+
+      response :default do
+        key :description, 'unexpected error'
+        schema do
+          key :'$ref', :ErrorModel
+        end
+      end
+    end
+  end
+
   # ----------------------------------------------------------------------------------------------------------
   # Swagger-Docs DSL [END]
   # ----------------------------------------------------------------------------------------------------------
-
   doorkeeper_for :all  # Require access token for all actions
 
   version 1
@@ -52,10 +113,12 @@ class Api::V1::VersesController < Api::V1::ApiController
   # until it's modified (using Efficient Validation)
   caches :index, :show, :caches_for => 15.minutes
 
+  # GET /verses/{ID}
   def show
     expose Verse.find( params[:id] )
   end
 
+  # GET /verses/lookup
   def lookup
     tl = params[:tl] ? params[:tl] : current_resource_owner.translation
     expose Verse.exists_in_db(params[:bk], params[:ch], params[:vs], tl)
