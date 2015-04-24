223399b99ce47e11acbca591e7821eeb9d18a000
Andy <avitus@gmail.com>
2015-04-24 16:56:10 -0700

    Tweaks to JSON

--- app/models/memverse.rb
+++ app/models/memverse.rb
@@ -47,11 +47,11 @@ class Memverse < ActiveRecord::Base
     end    
     property :created_at do
       key :type, :string
-      key :format, :dateTime
+      key :format, :date-time
     end 
     property :updated_at do
       key :type, :string
-      key :format, :dateTime
+      key :format, :date-time
     end 
     property :first_verse do
       key :type, :integer
