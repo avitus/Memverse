313fb9b87a42ec65a9ff6fda41477a1bdb125672
Andy <avitus@gmail.com>
2016-04-05 16:28:51 -0700

    Add book and chapter to API for memverse model

--- app/models/memverse.rb
+++ app/models/memverse.rb
@@ -175,7 +175,7 @@ class Memverse < ActiveRecord::Base
   def serializable_hash(options = {})
     super only: [:id, :user_id, :next_test, :test_interval, :status, :rep_n, :efactor, :subsection, :prev_verse, :passage_id, :next_ref_test, :ref_interval],
           methods: [:ref],
-          include: [verse: {only: [:id, :translation, :text, :versenum]}]
+          include: [verse: {only: [:id, :translation, :text, :book_index, :book, :chapter, :versenum]}]
   end
 
   # Convert to JSON format (for AJAX goodness on main memorization page)
