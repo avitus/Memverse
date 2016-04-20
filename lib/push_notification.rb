b7a4bdfd68b28203b48c8daad7c9798968b030ad
Andy <avitus@gmail.com>
2016-04-20 16:10:12 -0700

    Update push notification gem (Rpush)

--- lib/push_notification.rb
+++ lib/push_notification.rb
@@ -2,7 +2,7 @@
 
 module PushNotification
 
-  def ios_push(msg)
+  def ios_quiz_alert(msg)
 
     app_name = Rpush::Apns::App.find_by_name("Memverse_iOS")  # name of the mobile app
 
@@ -18,6 +18,28 @@ module PushNotification
         Rails.logger.info("   Sent iOS push notification to #{u.email}")
       end
     }
+
+    Rpush.push  # send all push notifications
+
+  end
+
+  # Not yet using this
+  def mobile_reminder(msg, user_id)
+
+    app_name = Rpush::Apns::App.find_by_name("Memverse_iOS")  # name of the mobile app
+
+    u = User.find(user_id)
+
+    if u.device_token && u.device_token.length == 64
+      n               = Rpush::Apns::Notification.new
+      n.app           = app_name
+      n.device_token  = u.device_token # 64-character hex string
+      n.alert         = msg
+      n.save!
+      Rails.logger.info("   Sent iOS reminder push notification to #{u.email}")
+    else
+      Rails.logger.info("   Invalide device token for push notification for #{u.email}")
+    end    
 
     Rpush.push  # send all push notifications
 
