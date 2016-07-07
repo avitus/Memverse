9ba3cf2813dd57230f6c55a3bd0359c3d5ba5331
Andy <avitus@gmail.com>
2016-07-07 10:09:02 -0700

    Explicitly sets Redis URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,10 @@
 if Rails.env = 'production'
 
 	# In production Redis is on localhost listening on port 6379
-	$redis = Redis.new
+	$redis = Redis.new(:host => 'localhost', :port => 6379)
 
 else
-	
+
 	# In dev/test we will be using a docker container called 'redis'
 	$redis = Redis.new(:host => (ENV['WERCKER_REDIS_HOST'] || 'redis'), :port => 6379)
 
