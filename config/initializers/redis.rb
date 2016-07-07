be6a30963217813e44674a35ba551bd911533f8f
Andy <avitus@gmail.com>
2016-07-07 09:48:44 -0700

    Different configuration for Redis in production environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-$redis = Redis.new(:host => (ENV['WERCKER_REDIS_HOST'] || 'redis'), :port => 6379)
+if Rails.env = 'production'
+
+	# In production Redis is on localhost listening on port 6379
+	$redis = Redis.new
+
+else
+	
+	# In dev/test we will be using a docker container called 'redis'
+	$redis = Redis.new(:host => (ENV['WERCKER_REDIS_HOST'] || 'redis'), :port => 6379)
+
+end
