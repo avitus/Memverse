666a34de40d404a22ca2bd19ce3212035d46ed56
Andy <avitus@gmail.com>
2016-10-10 21:07:16 -0700

    Add Swagger schema

--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -1,5 +1,41 @@
 class ProgressReport < ActiveRecord::Base
 
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Blocks DSL [START]
+  # ----------------------------------------------------------------------------------------------------------
+  include Swagger::Blocks
+
+  swagger_schema :ProgressReport do
+    key :required, [:user_id, :entry_date, :learning, :memorized, :time_allocation, :consistency]
+    property :user_id do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :entry_date do
+      key :type, :string
+      key :format, :date
+    end 
+    property :learning do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :memorized do
+      key :type, :integer
+      key :format, :int64
+    end  
+    property :time_allocation do
+      key :type, :integer
+      key :format, :int64
+    end  
+    property :consistency do
+      key :type, :integer
+      key :format, :int64
+    end           
+  end
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Blocks DSL [END]
+  # ----------------------------------------------------------------------------------------------------------
+
   #  Structure
   #  t.integer   :user_id,     :null => false
   #  t.date      :entry_date
