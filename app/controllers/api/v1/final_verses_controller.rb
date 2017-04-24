16506b862350046a4afa30a4cf8349c5fa71f550
Andy <avitus@gmail.com>
2017-04-24 15:24:20 -0700

    Add final verse table to API

---
+++ app/controllers/api/v1/final_verses_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::FinalVersesController < Api::V1::ApiController
+
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Blocks DSL [START]
+  # ----------------------------------------------------------------------------------------------------------
+  include Swagger::Blocks
+
+  swagger_path '/final_verses' do
+
+    operation :get do
+      key :description, 'Returns final verse for each chapter of the Bible'
+      key :operationId, 'showFinalVerses'
+      key :tags, ['final_verses']
+      security do
+        key :oauth2, ['admin write read public']
+      end
+      response 200 do
+        key :description, 'Final verse response'
+        schema do
+          key :'$ref', :FinalVerse
+        end
+      end
+      response 401 do
+        key :description, 'Unauthorized response'
+        schema do
+          key :'$ref', :FinalVerse
+        end
+      end
+      response :default do
+        key :description, 'Unexpected error'
+        schema do
+          key :'$ref', :ErrorModel
+        end
+      end
+    end
+
+  end
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Docs DSL [END]
+  # ----------------------------------------------------------------------------------------------------------
+
+  before_action only: [:index] do
+    doorkeeper_authorize! :admin, :write, :read, :public # allow any of these scopes access (logical OR)
+  end
+
+  version 1
+
+  def index
+    expose FinalVerse.for_api
+  end
+
+end
--- app/models/final_verse.rb
+++ app/models/final_verse.rb
@@ -1,5 +1,26 @@
 class FinalVerse < ActiveRecord::Base
 
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Blocks DSL [START]
+  # ----------------------------------------------------------------------------------------------------------
+  include Swagger::Blocks
+
+  swagger_schema :FinalVerse do
+    property :book do
+      key :type, :string
+    end
+    property :chapter do
+      key :type, :integer
+      key :format, :int64
+    end 
+    property :last_verse do
+      key :type, :integer
+      key :format, :int64
+    end           
+  end
+  # ----------------------------------------------------------------------------------------------------------
+  # Swagger-Blocks DSL [END]
+  # ----------------------------------------------------------------------------------------------------------
   
   # Validations
   validates_presence_of :book, :chapter, :last_verse
