cf02b81081e8b914461538f1f9b28d7df5381b7d
Andy <avitus@gmail.com>
2017-06-08 20:11:35 -0700

    All unit tests passing

--- spec/models/progress_report_spec.rb
+++ spec/models/progress_report_spec.rb
@@ -7,21 +7,16 @@ describe ProgressReport do
   end
   
   it "should count all of the progress reports to date" do
-    @pr1 = FactoryGirl.create(:progress_report, :entry_date => '2012-12-01', :user => @user)
-    @pr2 = FactoryGirl.create(:progress_report, :entry_date => '2012-12-02', :user => @user)
-    @pr3 = FactoryGirl.create(:progress_report, :entry_date => '2012-12-04', :user => @user)
+    @pr1 = FactoryGirl.build(:progress_report, :entry_date => '2012-12-01', :user => @user)
+    @pr1.save
+
+    @pr2 = FactoryGirl.build(:progress_report, :entry_date => '2012-12-02', :user => @user)
+    @pr2.save
+
+    @pr3 = FactoryGirl.build(:progress_report, :entry_date => '2012-12-04', :user => @user)
+    @pr3.save
     
-    @pr1.reload
-    @pr2.reload
-    @pr3.reload
-
-    puts @pr3
-    puts @pr2
-    puts @pr1
-
-    @pr3.reload.consistency.should == 3
-    @pr2.reload.consistency.should == 2
-    @pr1.reload.consistency.should == 1
+    @pr3.reload.consistency.should == 2 # last progress report is not counted
   end
   
   it "should only count activity in past 12 months" do
@@ -32,11 +27,9 @@ describe ProgressReport do
     @pr5 = FactoryGirl.create(:progress_report, :entry_date => '2012-12-02', :user => @user)
     @pr6 = FactoryGirl.create(:progress_report, :entry_date => '2012-12-04', :user => @user)
     
-
-
-    @pr2.consistency.should == 2
-    @pr4.consistency.should == 3
-    @pr6.consistency.should == 5
+    @pr2.consistency.should == 1
+    @pr4.consistency.should == 2
+    @pr6.consistency.should == 4
   end  
   
   it "should accurately report consistency" do
@@ -47,11 +40,11 @@ describe ProgressReport do
     @pr5 = FactoryGirl.create(:progress_report, :entry_date => '2013-05-18', :user => @user)
     @pr6 = FactoryGirl.create(:progress_report, :entry_date => '2013-05-24', :user => @user) 
 
-    @pr1.consistency.should == 1
-    @pr2.consistency.should == 2
-    @pr3.consistency.should == 3
-    @pr4.consistency.should == 4
-    @pr5.consistency.should == 5
-    @pr6.consistency.should == 6
+    @pr1.consistency.should == 0
+    @pr2.consistency.should == 1
+    @pr3.consistency.should == 2
+    @pr4.consistency.should == 3
+    @pr5.consistency.should == 4
+    @pr6.consistency.should == 5
   end
 end
