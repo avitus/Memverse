673cbf13ee04907fe1e73a4f62136efcb3421c5f
Andy <avitus@gmail.com>
2017-12-08 16:02:20 -0800

    Wait for a looong time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -104,7 +104,7 @@ Capybara.server = :puma
 Capybara.register_driver :selenium do |app|
   profile = Selenium::WebDriver::Firefox::Profile.new
   client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 120 # instead of the default 60
+  client.timeout = 600 # instead of the default 60
   Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, http_client: client)
 end
 
