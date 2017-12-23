0b728c8666aac8cca15c61877ea4480d228f489d
Andy <avitus@gmail.com>
2017-12-23 10:44:05 -0800

    Add PubNub keys

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,27 +1,11 @@
-if Rails.env.development?
-
-  PN = Pubnub.new(
-      :publish_key   => 'pub-c-56b243c5-5d07-457a-a290-fa4a29b1ff83', # publish_key only required if publishing.
-      :subscribe_key => 'sub-c-88c2a812-47e6-11e3-8c4f-02ee2ddab7fe', # required
-      :secret_key    => nil,                                          # optional, if used, message signing is enabled
-      :cipher_key    => nil,                                          # optional, if used, encryption is enabled
-      :ssl           => true,                                         # true or default is false
-      :logger        => Logger.new('log/pubnub.log', 'monthly')       # use the standard logger
-  )
-
-else
-
-  # Use production key by default
-  PN = Pubnub.new(
-      :publish_key   => 'pub-c-dc9e4561-d42a-4270-84b9-a9f268cd2cd2', # publish_key only required if publishing.
-      :subscribe_key => 'sub-c-bcc87aee-e8b7-11e2-acbe-02ee2ddab7fe', # required
-      :secret_key    => nil,                                          # optional, if used, message signing is enabled
-      :cipher_key    => nil,                                          # optional, if used, encryption is enabled
-      :ssl           => true,                                         # true or default is false
-      :logger        => Logger.new('log/pubnub.log', 'monthly')       # use the standard logger
-  )
-
-end
+PN = Pubnub.new(
+    :publish_key   => Rails.application.secrets[:pubnub_publish],   # publish_key only required if publishing.
+    :subscribe_key => Rails.application.secrets[:pubnub_subscribe], # required
+    :secret_key    => nil,                                          # optional, if used, message signing is enabled
+    :cipher_key    => nil,                                          # optional, if used, encryption is enabled
+    :ssl           => true,                                         # true or default is false
+    :logger        => Logger.new('log/pubnub.log', 'monthly')       # use the standard logger
+)
 
 PN_CALLBACK = lambda { |envelope|
   if envelope.error
