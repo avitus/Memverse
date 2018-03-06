5c7506ed97964816224860850a9e6ec14ede64b3
Andy <avitus@gmail.com>
2018-03-06 14:48:14 -0800

    No longer encrypt publish and subscribe keys

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,6 +1,7 @@
 PN = Pubnub.new(
-    publish_key:   Rails.application.secrets[:pubnub_publish],   # publish_key only required if publishing.
-    subscribe_key: Rails.application.secrets[:pubnub_subscribe], # required
+    # These are the regular PubNub Memverse keys. The /chat page uses the ChatEngine keys
+    publish_key:   'pub-c-dc9e4561-d42a-4270-84b9-a9f268cd2cd2', # publish_key only required if publishing.
+    subscribe_key: 'sub-c-bcc87aee-e8b7-11e2-acbe-02ee2ddab7fe', # required
     secret_key:    nil,                                          # optional, if used, message signing is enabled
     cipher_key:    nil,                                          # optional, if used, encryption is enabled
     ssl:           true,                                         # true or default is false
