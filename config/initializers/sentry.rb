8bed5595741d3db8d44c3afe209267818c428a0e
Andy <avitus@gmail.com>
2018-03-07 13:02:19 -0800

    Only report errors in production environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,5 @@
     Raven.configure do |config|
       config.dsn = 'https://a1106f25de724396a866c6ab9386b11b:cead6cb038b44236b6e3e43887faf76d@sentry.io/299442'
       config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+      config.environments = %w[ production ]
     end
