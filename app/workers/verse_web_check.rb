1e23783363013770a1fb6369e457aa2c0b6e0ff0
Andy <avitus@gmail.com>
2018-03-13 11:00:55 -0700

    Better logging for verse web check

--- app/workers/verse_web_check.rb
+++ app/workers/verse_web_check.rb
@@ -2,10 +2,20 @@ class VerseWebCheck
   include Sidekiq::Worker
 
   def perform(id)
-    vs = Verse.find(id)
 
-    if vs.web_text == vs.database_text
-      vs.update_column(:verified, true)
-    end
+    vs = Verse.find_by_id(id)
+
+    if vs 
+    	if vs.web_text == vs.database_text
+      	vs.update_column(:verified, true)
+      	Sidekiq.logger.info "Auto-verified verse with ID: " + id
+    	else
+    		Sidekiq.logger.info "Verse text did not match web for verse with ID: " + id
+    	end
+		else
+			Sidekiq.logger.warn "Unable to find verse with ID: " + id
+		end
+  
   end
-end
+
+end # class
