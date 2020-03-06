c18bb9516fed96ed226bc9e974b8c28340aa49bc
Andy <avitus@gmail.com>
2020-03-06 15:57:44 -0800

    Remove deprecated File.exists?

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|  
   schedule_file = "config/sidekiq_schedule.yml"
 
-  if File.exists?(schedule_file) && Sidekiq.server?
+  if File.exist?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
   end
 end  
