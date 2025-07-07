0807e38398c22118e8f46502d59553f973e91335
Andy Vitus <avitus@gmail.com>
2025-07-06 21:43:27 -0700

    Better handling of Ckeditor loading

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,18 +1,22 @@
 # Use this hook to configure ckeditor
-if Object.const_defined?("Ckeditor")
-  Ckeditor.setup do |config|
-    # ==> ORM configuration
-    # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
-    # :mongoid (bson_ext recommended) by default. Other ORMs may be
-    # available as additional gems.
-    require "ckeditor/orm/active_record"
+begin
+  if defined?(Ckeditor)
+    Ckeditor.setup do |config|
+      # ==> ORM configuration
+      # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
+      # :mongoid (bson_ext recommended) by default. Other ORMs may be
+      # available as additional gems.
+      require "ckeditor/orm/active_record"
 
-    # Allowed image file types for upload.
-    # Set to nil or [] (empty array) for all file types
-    # config.image_file_types = ["jpg", "jpeg", "png", "gif", "tiff"]
+      # Allowed image file types for upload.
+      # Set to nil or [] (empty array) for all file types
+      # config.image_file_types = ["jpg", "jpeg", "png", "gif", "tiff"]
 
-    # Allowed attachment file types for upload.
-    # Set to nil or [] (empty array) for all file types
-    # config.attachment_file_types = ["doc", "docx", "xls", "odt", "ods", "pdf", "rar", "zip", "tar", "swf"]
+      # Allowed attachment file types for upload.
+      # Set to nil or [] (empty array) for all file types
+      # config.attachment_file_types = ["doc", "docx", "xls", "odt", "ods", "pdf", "rar", "zip", "tar", "swf"]
+    end
   end
+rescue NameError => e
+  Rails.logger.warn "CKEditor not loaded: #{e.message}"
 end
