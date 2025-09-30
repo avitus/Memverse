904a50b765975b8a97409134e52e9564563c9e6f
Andy Vitus <avitus@gmail.com>
2025-09-30 16:02:39 -0700

    Display quiz badge correctly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_19_223753) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_30_222702) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -466,7 +466,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_223753) do
     t.index ["user_id"], name: "index_progress_reports_on_user_id"
   end
 
-  create_table "quests", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+  create_table "quests", id: :integer, charset: "latin1", force: :cascade do |t|
     t.integer "level"
     t.string "task"
     t.text "description"
@@ -1009,6 +1009,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_223753) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "thredded_messageboard_users", "thredded_messageboards", on_delete: :cascade
+  add_foreign_key "thredded_messageboard_users", "thredded_user_details", on_delete: :cascade
   add_foreign_key "thredded_user_post_notifications", "thredded_posts", column: "post_id", on_delete: :cascade
   add_foreign_key "thredded_user_post_notifications", "users", on_delete: :cascade
 end
