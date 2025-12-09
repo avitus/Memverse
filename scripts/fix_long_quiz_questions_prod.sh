7fa04cbf2d4c4c708a8fe9cdef6f721a42a7d1af
Andy Vitus <avitus@gmail.com>
2025-12-09 15:41:44 -0800

    More unnecessary files

--- scripts/fix_long_quiz_questions_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to fix long quiz questions in production
-
-echo "Running fix_long_quiz_questions.rb in production environment..."
-RAILS_ENV=production bundle exec rails runner fix_long_quiz_questions.rb
